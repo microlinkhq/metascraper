76bb282ddb39ac267950a9cf7fb5b1453d5f2db2
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-07-01 18:20:21 +0200

    Publisher refactor

--- src/rules/publisher.js
+++ src/rules/publisher.js
@@ -1,3 +1,6 @@
+'use strict'
+
+const isString = require('lodash.isstring')
 
 /**
  * Wrap a rule with validation and formatting logic.
@@ -6,17 +9,15 @@
  * @return {Function} wrapped
  */
 
-function wrap(rule) {
-  return ($) => {
-    let value = rule($)
-    if (typeof value != 'string') return
+const wrap = rule => $ => {
+  let value = rule($)
+  if (!isString(value)) return
 
-    // remove whitespace and new lines
-    value = value.trim()
-    value = value.replace(/\n/mg, '')
+  // remove whitespace and new lines
+  value = value.trim()
+  value = value.replace(/\n/gm, '')
 
-    return value
-  }
+  return value
 }
 
 /**
@@ -24,31 +25,31 @@ function wrap(rule) {
  */
 
 module.exports = [
-  wrap(($) => $('meta[property="og:site_name"]').attr('content')),
-  wrap(($) => $('meta[name="application-name"]').attr('content')),
-  wrap(($) => $('meta[property="al:android:app_name"]').attr('content')),
-  wrap(($) => $('meta[property="al:iphone:app_name"]').attr('content')),
-  wrap(($) => $('meta[property="al:ipad:app_name"]').attr('content')),
-  wrap(($) => $('meta[name="publisher"]').attr('content')),
-  wrap(($) => $('meta[name="Publisher"]').attr('content')),
-  wrap(($) => $('meta[name="twitter:app:name:iphone"]').attr('content')),
-  wrap(($) => $('meta[name="twitter:app:name:ipad"]').attr('content')),
-  wrap(($) => $('meta[name="twitter:app:name:googleplay"]').attr('content')),
-  wrap(($) => $('#logo').text()),
-  wrap(($) => $('.logo').text()),
-  wrap(($) => $('a[class*="brand"]').text()),
-  wrap(($) => $('[class*="brand"]').text()),
-  wrap(($) => $('[class*="logo"] a img[alt]').attr('alt')),
-  wrap(($) => $('[class*="logo"] img[alt]').attr('alt')),
-  wrap(($) => {
+  wrap($ => $('meta[property="og:site_name"]').attr('content')),
+  wrap($ => $('meta[name="application-name"]').attr('content')),
+  wrap($ => $('meta[property="al:android:app_name"]').attr('content')),
+  wrap($ => $('meta[property="al:iphone:app_name"]').attr('content')),
+  wrap($ => $('meta[property="al:ipad:app_name"]').attr('content')),
+  wrap($ => $('meta[name="publisher"]').attr('content')),
+  wrap($ => $('meta[name="Publisher"]').attr('content')),
+  wrap($ => $('meta[name="twitter:app:name:iphone"]').attr('content')),
+  wrap($ => $('meta[name="twitter:app:name:ipad"]').attr('content')),
+  wrap($ => $('meta[name="twitter:app:name:googleplay"]').attr('content')),
+  wrap($ => $('#logo').text()),
+  wrap($ => $('.logo').text()),
+  wrap($ => $('a[class*="brand"]').text()),
+  wrap($ => $('[class*="brand"]').text()),
+  wrap($ => $('[class*="logo"] a img[alt]').attr('alt')),
+  wrap($ => $('[class*="logo"] img[alt]').attr('alt')),
+  wrap($ => {
     let title = $('title').text().trim()
     let regexp = /^.*?\|\s+(.*)$/
     let matches = regexp.exec(title)
     if (!matches) return
     return matches[1]
   }),
-  wrap(($) => $('[itemtype="http://schema.org/Blog"] [itemprop="name"]').attr('content')),
-  wrap(($) => {
+  wrap($ => $('[itemtype="http://schema.org/Blog"] [itemprop="name"]').attr('content')),
+  wrap($ => {
     let desc = $('link[rel="alternate"][type="application/atom+xml"]').attr('title')
     let regexp = /^(.*?)\s[-|]\satom$/i
     let matches = regexp.exec(desc)
