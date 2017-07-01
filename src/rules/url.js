2337f7097e0c49e262a8f86f64b09cad44ecaa02
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-07-01 18:26:46 +0200

    Refactor url

--- src/rules/url.js
+++ src/rules/url.js
@@ -1,5 +1,10 @@
+'use strict'
 
-let isUrl = require('is-url')
+const normalizeUrl = require('normalize-url')
+const isString = require('lodash.isstring')
+const urlRegex = require('url-regex')
+
+const isUrl = value => urlRegex().test(value)
 
 /**
  * Wrap a rule with validation and formatting logic.
@@ -8,17 +13,17 @@ let isUrl = require('is-url')
  * @return {Function} wrapped
  */
 
-function wrap(rule) {
-  return ($) => {
-    let value = rule($)
-    if (typeof value != 'string') return
+const wrap = rule => $ => {
+  let value = rule($)
+  if (!isString(value)) return
 
-    // make sure it's a url
-    value = value.trim()
-    if (!isUrl(value)) return
+  // make sure it's a url
+  value = value.trim()
+  if (!isUrl(value)) return
 
-    return value
-  }
+  return normalizeUrl(value, {
+    stripWWW: false
+  })
 }
 
 /**
@@ -26,8 +31,8 @@ function wrap(rule) {
  */
 
 module.exports = [
-  wrap(($) => $('meta[property="og:url"]').attr('content')),
-  wrap(($) => $('meta[name="twitter:url"]').attr('content')),
-  wrap(($) => $('link[rel="canonical"]').attr('href')),
-  wrap(($, url) => url),
+  wrap($ => $('meta[property="og:url"]').attr('content')),
+  wrap($ => $('meta[name="twitter:url"]').attr('content')),
+  wrap($ => $('link[rel="canonical"]').attr('href')),
+  wrap(($, url) => url)
 ]
