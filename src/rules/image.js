6eb017fe36562ec18151f9a7342a8423d1c68c0b
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-07-01 18:16:46 +0200

    Refactor image

--- src/rules/image.js
+++ src/rules/image.js
@@ -1,5 +1,9 @@
+'use strict'
 
-let isUrl = require('is-url')
+const normalizeUrl = require('normalize-url')
+const urlRegex = require('url-regex')
+
+const isUrl = value => urlRegex().test(value)
 
 /**
  * Wrap a rule with validation and formatting logic.
@@ -8,12 +12,13 @@ let isUrl = require('is-url')
  * @return {Function} wrapped
  */
 
-function wrap(rule) {
-  return ($) => {
-    let value = rule($)
-    if (!isUrl(value)) return
-    return value
-  }
+const wrap = rule => $ => {
+  let value = rule($)
+
+  if (!isUrl(value)) return
+  return normalizeUrl(value, {
+    stripWWW: false
+  })
 }
 
 /**
@@ -21,18 +26,18 @@ function wrap(rule) {
  */
 
 module.exports = [
-  wrap(($) => $('meta[property="og:image:secure_url"]').attr('content')),
-  wrap(($) => $('meta[property="og:image:url"]').attr('content')),
-  wrap(($) => $('meta[property="og:image"]').attr('content')),
-  wrap(($) => $('meta[name="twitter:image"]').attr('content')),
-  wrap(($) => $('meta[property="twitter:image"]').attr('content')),
-  wrap(($) => $('meta[name="twitter:image:src"]').attr('content')),
-  wrap(($) => $('meta[property="twitter:image:src"]').attr('content')),
-  wrap(($) => $('meta[name="sailthru.image"]').attr('content')),
-  wrap(($) => $('meta[name="sailthru.image.full"]').attr('content')),
-  wrap(($) => $('meta[name="sailthru.image.thumb"]').attr('content')),
-  wrap(($) => $('article img[src]').first().attr('src')),
-  wrap(($) => $('#content img[src]').first().attr('src')),
-  wrap(($) => $('[class*="article"] img[src]').first().attr('src')),
-  wrap(($) => $('img[src]').first().attr('src')),
+  wrap($ => $('meta[property="og:image:secure_url"]').attr('content')),
+  wrap($ => $('meta[property="og:image:url"]').attr('content')),
+  wrap($ => $('meta[property="og:image"]').attr('content')),
+  wrap($ => $('meta[name="twitter:image"]').attr('content')),
+  wrap($ => $('meta[property="twitter:image"]').attr('content')),
+  wrap($ => $('meta[name="twitter:image:src"]').attr('content')),
+  wrap($ => $('meta[property="twitter:image:src"]').attr('content')),
+  wrap($ => $('meta[name="sailthru.image"]').attr('content')),
+  wrap($ => $('meta[name="sailthru.image.full"]').attr('content')),
+  wrap($ => $('meta[name="sailthru.image.thumb"]').attr('content')),
+  wrap($ => $('article img[src]').first().attr('src')),
+  wrap($ => $('#content img[src]').first().attr('src')),
+  wrap($ => $('[class*="article"] img[src]').first().attr('src')),
+  wrap($ => $('img[src]').first().attr('src'))
 ]
