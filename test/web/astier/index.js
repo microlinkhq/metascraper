fdebba635ae3d4d4ef29162c365695b71cf98cf3
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-07-10 18:20:23 +0200

    Add astier case

---
+++ test/web/astier/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const {load: loadJSON} = require('json-future')
+const {resolve} = require('path')
+const {promisify} = require('util')
+const should = require('should')
+const fs = require('fs')
+
+const getMetaData = require('../../..')
+const readFile = promisify(fs.readFile)
+
+const url = 'https://anisse.astier.eu'
+
+it('astier', async () => {
+  const html = await readFile(resolve(__dirname, 'input.html'))
+  const json = await loadJSON(resolve(__dirname, 'output.json'))
+  const metadata = await getMetaData({html, url})
+  should(metadata).be.eql(json)
+})
