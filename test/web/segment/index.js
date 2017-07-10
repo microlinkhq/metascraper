cc1d14795ccf6efe91dceec369b28ca8c4cda8f8
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-07-10 18:30:50 +0200

    Update nullable URL's

--- test/web/segment/index.js
+++ test/web/segment/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const {load: loadJSON} = require('json-future')
-const {resolve, basename} = require('path')
+const {resolve} = require('path')
 const {promisify} = require('util')
 const should = require('should')
 const fs = require('fs')
@@ -9,9 +9,11 @@ const fs = require('fs')
 const getMetaData = require('../../..')
 const readFile = promisify(fs.readFile)
 
-it(basename(__dirname), async () => {
+const url = 'https://segment.com/blog/scaling-nsq/'
+
+it('segment', async () => {
   const html = await readFile(resolve(__dirname, 'input.html'))
   const json = await loadJSON(resolve(__dirname, 'output.json'))
-  const metadata = await getMetaData({html})
+  const metadata = await getMetaData({html, url})
   should(metadata).be.eql(json)
 })
--- test/web/startup-grind/index.js
+++ test/web/startup-grind/index.js
@@ -1,17 +1,18 @@
 'use strict'
 
 const {load: loadJSON} = require('json-future')
-const {resolve, basename} = require('path')
+const {resolve} = require('path')
 const {promisify} = require('util')
 const should = require('should')
 const fs = require('fs')
 
 const getMetaData = require('../../..')
 const readFile = promisify(fs.readFile)
+const url = 'https://www.startupgrind.com/blog/tech-funding-is-officialy-slowing-down-3-ways-to-survive-the-cooling/'
 
-it(basename(__dirname), async () => {
+it('startup-grind', async () => {
   const html = await readFile(resolve(__dirname, 'input.html'))
   const json = await loadJSON(resolve(__dirname, 'output.json'))
-  const metadata = await getMetaData({html})
+  const metadata = await getMetaData({html, url})
   should(metadata).be.eql(json)
 })
