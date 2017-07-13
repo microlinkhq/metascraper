fefd105f71fca1eb69f2f0818ef3664f3726c8f9
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-07-13 17:43:38 +0200

    Add wired case

---
+++ test/web/wired/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const {load: loadJSON} = require('json-future')
+const {resolve} = require('path')
+const {promisify} = require('util')
+const should = require('should')
+const fs = require('fs')
+
+const getMetaData = require('../../..')
+const readFile = promisify(fs.readFile)
+
+it('wired', async () => {
+  const html = await readFile(resolve(__dirname, 'input.html'))
+  const json = await loadJSON(resolve(__dirname, 'output.json'))
+  const metadata = await getMetaData({html})
+  should(metadata).be.eql(json)
+})
