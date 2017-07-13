2522915319c2739384eecef3be9103efcff95d32
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-07-13 17:34:16 +0200

    Add instagram

--- src/get-data/rules/url.js
+++ src/get-data/rules/url.js
@@ -26,5 +26,6 @@ module.exports = [
   wrap($ => $('meta[property="og:url"]').attr('content')),
   wrap($ => $('meta[name="twitter:url"]').attr('content')),
   wrap($ => $('link[rel="canonical"]').attr('href')),
+  wrap($ => $('link[rel="alternate"][hreflang="x-default"]').attr('href')),
   ($, url) => isString(url) ? normalizeUrl(url) : null
 ]
