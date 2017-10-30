4a27d08228557f0c550ad83f577623354076ddf4
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-10-30 15:40:04 +0100

    Add pikabu test

---
+++ test/web/pikabu/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const snapshot = require('snap-shot')
+const {promisify} = require('util')
+const {resolve} = require('path')
+
+const fs = require('fs')
+
+const getMetaData = require('../../..')
+const readFile = promisify(fs.readFile)
+
+const url = 'https://pikabu.ru'
+
+it('nytimes', async () => {
+  const html = await readFile(resolve(__dirname, 'input.html'))
+  const metadata = await getMetaData({html, url})
+  console.log(metadata)
+  snapshot(metadata)
+})
