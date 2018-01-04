3fc613006d7832f6d8a5404c69d8bd5fd06f8dc9
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-01-04 20:04:36 +0100

    Avoid non deterministic date

--- packages/metascraper-amazon/test/index.js
+++ packages/metascraper-amazon/test/index.js
@@ -5,6 +5,7 @@ const snapshot = require('snap-shot')
 const { promisify } = require('util')
 const { resolve } = require('path')
 const { omit } = require('lodash')
+const should = require('should')
 const fs = require('fs')
 
 const readFile = promisify(fs.readFile)
@@ -35,16 +36,22 @@ describe('metascraper amazon integration', () => {
       const metascraper = require('metascraper')
       const html = await readFile(resolve(__dirname, 'fixtures/amazon-com/ansi-url.html'))
       const url = 'https://www.amazon.com/gp/product/B0057OC5O8/'
-      const meta = omit(await metascraper({ html, url }), ['date'])
-      snapshot(meta)
+      const metadata = await metascraper({ html, url })
+
+      // omit date because it is non deterministic
+      snapshot(omit(metadata, ['date']))
+      should(metadata.date).instanceOf(String)
     })
 
     it('product url', async () => {
       const metascraper = require('metascraper')
       const html = await readFile(resolve(__dirname, 'fixtures/amazon-com/product-url.html'))
       const url = 'https://www.amazon.com/The-Whole-Truth-Shaw-Book-ebook/dp/B0011UCPM4/ref=pd_zg_rss_ts_b_17_6?ie=UTF8&tag=recomshop-22'
-      const meta = omit(await metascraper({ html, url }), ['date'])
-      snapshot(meta)
+      const metadata = await metascraper({ html, url })
+
+      // omit date because it is non deterministic
+      snapshot(omit(metadata, ['date']))
+      should(metadata.date).instanceOf(String)
     })
   })
 
@@ -53,8 +60,11 @@ describe('metascraper amazon integration', () => {
       const metascraper = require('metascraper')
       const html = await readFile(resolve(__dirname, 'fixtures/amazon-es/product-url.html'))
       const url = 'https://www.amazon.es/aspirador-Excellence-Programable-limpieza-Silencioso/dp/B01MUGXRT9'
-      const meta = omit(await metascraper({ html, url }), ['date'])
-      snapshot(meta)
+      const metadata = await metascraper({ html, url })
+
+      // omit date because it is non deterministic
+      snapshot(omit(metadata, ['date']))
+      should(metadata.date).instanceOf(String)
     })
   })
 })
--- packages/metascraper/test/integration/youtube-channel/index.js
+++ packages/metascraper/test/integration/youtube-channel/index.js
@@ -3,6 +3,8 @@
 const snapshot = require('snap-shot')
 const { promisify } = require('util')
 const { resolve } = require('path')
+const { omit } = require('lodash')
+const should = require('should')
 
 const fs = require('fs')
 
@@ -14,5 +16,7 @@ const url = 'https://www.youtube.com/channel/UCzcRQ3vRNr6fJ1A9rqFn7QA'
 it('youtube channel', async () => {
   const html = await readFile(resolve(__dirname, 'input.html'))
   const metadata = await metascraper({ html, url })
-  snapshot(metadata)
+  // omit date because it is non deterministic
+  snapshot(omit(metadata, ['date']))
+  should(metadata.date).instanceOf(String)
 })
