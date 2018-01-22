ab03d975a402e1646c551bf8dcf87747536c019c
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-01-22 23:33:34 +0100

    Move soundcloud test

---
+++ packages/metascraper-soundcloud/test/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const clearModule = require('clear-module')
+const snapshot = require('snap-shot')
+const { promisify } = require('util')
+const { resolve } = require('path')
+const fs = require('fs')
+
+const readFile = promisify(fs.readFile)
+
+let metascraper
+
+describe('metascraper-soundcloud', () => {
+  before(() => {
+    clearModule('metascraper')
+    process.env.METASCRAPER_CONFIG_CWD = __dirname
+    metascraper = require('metascraper')
+  })
+
+  after(() => {
+    clearModule('metascraper')
+    delete process.env.METASCRAPER_CONFIG_CWD
+  })
+
+  it('song', async () => {
+    const html = await readFile(resolve(__dirname, 'fixtures/song.html'))
+    const url = 'https://soundcloud.com/beautybrainsp/beauty-brain-swag-bandicoot'
+
+    const metadata = await metascraper({ html, url })
+    snapshot(metadata)
+  })
+})
