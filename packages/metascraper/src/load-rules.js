327fae456c9596eb63968894dbf63cc822ec432b
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-02-03 13:03:27 +0100

    Expose load and autoload methods

--- packages/metascraper/src/load-rules.js
+++ packages/metascraper/src/load-rules.js
@@ -5,6 +5,8 @@ const config = require('cosmiconfig')('metascraper').load(cwd)
 const resolveFrom = require('resolve-from')
 
 const {
+  map,
+  memoize,
   findIndex,
   forEach,
   chain,
@@ -26,50 +28,53 @@ const DEFAULT_RULES = [
   'metascraper-url'
 ]
 
-let singletonConfig
-
-module.exports = () =>
-  singletonConfig ||
-  Promise.resolve(config).then(configFile => {
-    const rules = get(configFile, 'config.rules', DEFAULT_RULES)
-
-    singletonConfig = chain(rules)
-      .map(rule => {
-        let moduleName
-        let moduleConfig
-
-        if (isString(rule)) {
-          moduleName = rule
-        } else if (isArray(rule)) {
-          moduleName = rule[0]
-          moduleConfig = rule[1]
-        } else if (isObject(rule)) {
-          moduleName = Object.keys(rule)[0]
-          moduleConfig = rule[moduleName]
-        }
-
-        const modulePath = resolveFrom(cwd, moduleName)
-        return require(modulePath)(moduleConfig)
-      })
-      // merge rules with same props
-      .reduce((acc, rules) => {
-        forEach(rules, function (rule, propName) {
-          const index = findIndex(acc, item => item[propName])
-          if (index !== -1) {
-            acc[index][propName] = acc[index][propName].concat(rule)
-          } else {
-            acc.push({ [propName]: rule })
-          }
-        })
-        return acc
-      }, [])
-      // export an array interface, it's easier to iterate
-      .map(obj => {
-        const key = Object.keys(obj)[0]
-        const value = obj[key]
-        return [key, value]
-      })
-      .value()
-
-    return singletonConfig
+const load = rules =>
+  // export an array interface, it's easier to iterate
+  map(rules, obj => {
+    const key = Object.keys(obj)[0]
+    const value = obj[key]
+    return [key, value]
   })
+
+const autoload = async () => {
+  const configFile = await config
+  const rules = get(configFile, 'config.rules', DEFAULT_RULES)
+
+  return chain(rules)
+    .map(rule => {
+      let moduleName
+      let moduleConfig
+
+      if (isString(rule)) {
+        moduleName = rule
+      } else if (isArray(rule)) {
+        moduleName = rule[0]
+        moduleConfig = rule[1]
+      } else if (isObject(rule)) {
+        moduleName = Object.keys(rule)[0]
+        moduleConfig = rule[moduleName]
+      }
+
+      const modulePath = resolveFrom(cwd, moduleName)
+      return require(modulePath)(moduleConfig)
+    })
+    // merge rules with same props
+    .reduce((acc, rules) => {
+      forEach(rules, function (rule, propName) {
+        const index = findIndex(acc, item => item[propName])
+        if (index !== -1) {
+          acc[index][propName] = acc[index][propName].concat(rule)
+        } else {
+          acc.push({ [propName]: rule })
+        }
+      })
+      return acc
+    }, [])
+    .thru(load)
+    .value()
+}
+
+module.exports = {
+  autoload,
+  load
+}
