63ce77da1dd475563a08432d84ce26aafd8ce2b4
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-02-13 21:47:42 +0100

    Get the highest image size

--- packages/metascraper-youtube/index.js
+++ packages/metascraper-youtube/index.js
@@ -3,9 +3,22 @@
 const getVideoId = require('get-video-id')
 
 const { getValue, isUrl, titleize } = require('@metascraper/helpers')
+const isReachable = require('is-reachable')
 const { isString } = require('lodash')
+const pLocate = require('p-locate')
 
-const getThumbnailUrl = id => `https://i.ytimg.com/vi/${id}/maxresdefault.jpg`
+const THUMBAILS_RESOLUTIONS = [
+  'maxresdefault.jpg',
+  'sddefault.jpg',
+  'hqdefault.jpg',
+  'mqdefault.jpg',
+  'default.jpg'
+]
+
+const getThumbnailUrl = id => {
+  const urls = THUMBAILS_RESOLUTIONS.map(res => `https://img.youtube.com/vi/${id}/${res}`)
+  return pLocate(urls, isReachable)
+}
 
 const wrap = rule => ({ htmlDom }) => {
   const value = rule(htmlDom)
--- packages/metascraper-youtube/test/index.js
+++ packages/metascraper-youtube/test/index.js
@@ -22,27 +22,38 @@ const metascraper = require('metascraper').load([
 const readFile = promisify(fs.readFile)
 
 describe('metascraper-youtube', () => {
-  it('youtube video', async () => {
-    const html = await readFile(resolve(__dirname, 'fixtures/youtube-video.html'))
-    const url = 'https://www.youtube.com/watch?v=hwMkbaS_M_c'
+  describe('urls', () => {
+    it('youtube video', async () => {
+      const html = await readFile(resolve(__dirname, 'fixtures/youtube-video.html'))
+      const url = 'https://www.youtube.com/watch?v=hwMkbaS_M_c'
 
-    const metadata = await metascraper({ html, url })
-    snapshot(metadata)
+      const metadata = await metascraper({ html, url })
+      snapshot(metadata)
+    })
+
+    it('youtube video old', async () => {
+      const html = await readFile(resolve(__dirname, 'fixtures/youtube-video-old.html'))
+      const url = 'https://www.youtube.com/watch?v=GDRd-BFTYIg'
+
+      const metadata = await metascraper({ html, url })
+      snapshot(metadata)
+    })
+
+    it('youtube channel', async () => {
+      const html = await readFile(resolve(__dirname, 'fixtures/youtube-channel.html'))
+      const url = 'https://www.youtube.com/channel/UCzcRQ3vRNr6fJ1A9rqFn7QA'
+
+      const metadata = omit(await metascraper({ html, url }), ['date'])
+      snapshot(metadata)
+    })
   })
 
-  it('youtube video old', async () => {
-    const html = await readFile(resolve(__dirname, 'fixtures/youtube-video-old.html'))
-    const url = 'https://www.youtube.com/watch?v=GDRd-BFTYIg'
-
-    const metadata = await metascraper({ html, url })
-    snapshot(metadata)
-  })
-
-  it('youtube channel', async () => {
-    const html = await readFile(resolve(__dirname, 'fixtures/youtube-channel.html'))
-    const url = 'https://www.youtube.com/channel/UCzcRQ3vRNr6fJ1A9rqFn7QA'
-
-    const metadata = omit(await metascraper({ html, url }), ['date'])
-    snapshot(metadata)
+  describe('image size', () => {
+    it('get the high image size', async () => {
+      const html = await readFile(resolve(__dirname, 'fixtures/image-size.html'))
+      const url = 'https://www.youtube.com/watch?v=rXyKq7izYCQ'
+      const metadata = omit(await metascraper({ html, url }), ['date'])
+      snapshot(metadata)
+    })
   })
 })
