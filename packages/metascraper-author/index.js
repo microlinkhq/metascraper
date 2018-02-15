a54ba9f284a13c037ad8005958bfb609d459db00
Tyler Bell <belltyler@live.com>
2018-02-15 16:30:56 -0500

    Added Regex to Recognize Dates in Author

--- packages/metascraper-author/index.js
+++ packages/metascraper-author/index.js
@@ -4,6 +4,7 @@ const { getValue, isUrl, titleize } = require('@metascraper/helpers')
 const { isString } = require('lodash')
 
 const REGEX_STRICT = /^\S+\s+\S+/
+const REGEX_DATE = /\d{2,}/g
 
 /**
  * Wrap a rule with validation and formatting logic.
@@ -29,7 +30,7 @@ const wrap = rule => ({ htmlDom }) => {
 
 const strict = rule => $ => {
   const value = rule($)
-  return REGEX_STRICT.test(value) && value
+  return REGEX_STRICT.test(value) && !REGEX_DATE.test(value) &&value
 }
 
 /**
@@ -38,19 +39,19 @@ const strict = rule => $ => {
 
 module.exports = () => ({
   author: [
-    wrap($ => $('meta[property="author"]').attr('content')),
-    wrap($ => $('meta[property="article:author"]').attr('content')),
-    wrap($ => $('meta[name="author"]').attr('content')),
-    wrap($ => $('meta[name="sailthru.author"]').attr('content')),
-    wrap($ => getValue($, $('[rel="author"]'))),
-    wrap($ => getValue($, $('[itemprop*="author"] [itemprop="name"]'))),
-    wrap($ => getValue($, $('[itemprop*="author"]'))),
-    wrap($ => $('meta[property="book:author"]').attr('content')),
-    strict(wrap($ => getValue($, $('a[class*="author"]')))),
-    strict(wrap($ => getValue($, $('[class*="author"] a')))),
-    strict(wrap($ => getValue($, $('a[href*="/author/"]')))),
-    wrap($ => getValue($, $('a[class*="screenname"]'))),
-    strict(wrap($ => getValue($, $('[class*="author"]')))),
-    strict(wrap($ => getValue($, $('[class*="byline"]'))))
+    // wrap($ => $('meta[property="author"]').attr('content')),
+    // wrap($ => $('meta[property="article:author"]').attr('content')),
+    // wrap($ => $('meta[name="author"]').attr('content')),
+    // wrap($ => $('meta[name="sailthru.author"]').attr('content')),
+    // wrap($ => getValue($, $('[rel="author"]'))),
+    // wrap($ => getValue($, $('[itemprop*="author"] [itemprop="name"]'))),
+    // wrap($ => getValue($, $('[itemprop*="author"]'))),
+    // wrap($ => $('meta[property="book:author"]').attr('content')),
+    // strict(wrap($ => getValue($, $('a[class*="author"]')))),
+    // strict(wrap($ => getValue($, $('[class*="author"] a')))),
+    // strict(wrap($ => getValue($, $('a[href*="/author/"]')))),
+    // wrap($ => getValue($, $('a[class*="screenname"]'))),
+    // strict(wrap($ => getValue($, $('[class*="author"]')))),
+    // strict(wrap($ => getValue($, $('[class*="byline"]'))))
   ]
 })
