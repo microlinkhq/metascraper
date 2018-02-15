73a05189716f7e622b278d11b10832500403cde4
Tyler Bell <belltyler@live.com>
2018-02-15 16:33:13 -0500

    Update to Date Regex

--- packages/metascraper-author/index.js
+++ packages/metascraper-author/index.js
@@ -39,19 +39,19 @@ const strict = rule => $ => {
 
 module.exports = () => ({
   author: [
-    // wrap($ => $('meta[property="author"]').attr('content')),
-    // wrap($ => $('meta[property="article:author"]').attr('content')),
-    // wrap($ => $('meta[name="author"]').attr('content')),
-    // wrap($ => $('meta[name="sailthru.author"]').attr('content')),
-    // wrap($ => getValue($, $('[rel="author"]'))),
-    // wrap($ => getValue($, $('[itemprop*="author"] [itemprop="name"]'))),
-    // wrap($ => getValue($, $('[itemprop*="author"]'))),
-    // wrap($ => $('meta[property="book:author"]').attr('content')),
-    // strict(wrap($ => getValue($, $('a[class*="author"]')))),
-    // strict(wrap($ => getValue($, $('[class*="author"] a')))),
-    // strict(wrap($ => getValue($, $('a[href*="/author/"]')))),
-    // wrap($ => getValue($, $('a[class*="screenname"]'))),
-    // strict(wrap($ => getValue($, $('[class*="author"]')))),
-    // strict(wrap($ => getValue($, $('[class*="byline"]'))))
+    wrap($ => $('meta[property="author"]').attr('content')),
+    wrap($ => $('meta[property="article:author"]').attr('content')),
+    wrap($ => $('meta[name="author"]').attr('content')),
+    wrap($ => $('meta[name="sailthru.author"]').attr('content')),
+    wrap($ => getValue($, $('[rel="author"]'))),
+    wrap($ => getValue($, $('[itemprop*="author"] [itemprop="name"]'))),
+    wrap($ => getValue($, $('[itemprop*="author"]'))),
+    wrap($ => $('meta[property="book:author"]').attr('content')),
+    strict(wrap($ => getValue($, $('a[class*="author"]')))),
+    strict(wrap($ => getValue($, $('[class*="author"] a')))),
+    strict(wrap($ => getValue($, $('a[href*="/author/"]')))),
+    wrap($ => getValue($, $('a[class*="screenname"]'))),
+    strict(wrap($ => getValue($, $('[class*="author"]')))),
+    strict(wrap($ => getValue($, $('[class*="byline"]'))))
   ]
 })
