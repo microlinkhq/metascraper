e9d35b5b898235c3632eeeef2399ea0b91ee5791
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-05-09 13:37:27 +0200

    Use microlinkhq/normalize-url dependency

--- packages/metascraper-helpers/index.js
+++ packages/metascraper-helpers/index.js
@@ -1,20 +1,15 @@
 'use strict'
 
-const condenseWhitespace = require('condense-whitespace')
 const { trim, flow, isEmpty } = require('lodash')
+const condenseWhitespace = require('condense-whitespace')
 const isRelativeUrl = require('is-relative-url')
-const { resolve: resolveUrl, URL } = require('url')
+const { resolve: resolveUrl } = require('url')
+const sanetizeUrl = require('normalize-url')
 const smartquotes = require('smartquotes')
 const toTitle = require('to-title-case')
 const urlRegex = require('url-regex')
-const prependHttp = require('prepend-http')
 
 const REGEX_BY = /^[\s\n]*by|@[\s\n]*/i
-const REMOVE_QUERY_PARAMS = [/^utm_\w+/i]
-
-const testParameter = (name, filters) => {
-  return filters.some(filter => filter instanceof RegExp ? filter.test(name) : filter === name)
-}
 
 const urlTest = (url, {relative = true}) => relative
   ? isRelativeUrl(url) || urlRegex().test(url)
@@ -22,55 +17,16 @@ const urlTest = (url, {relative = true}) => relative
 
 const isUrl = (url, opts = {}) => !isEmpty(url) && urlTest(url, opts)
 
-const normalizeUrl = (str) => {
-  if (typeof str !== 'string') {
-    throw new TypeError('Expected a string')
-  }
+const normalizeUrl = url => sanetizeUrl(url, {
+  stripWWW: false,
+  sortQueryParameters: false
+})
 
-  // Prepend protocol
-  str = prependHttp(str.trim()).replace(/^\/\//, 'http://')
-
-  const urlObj = new URL(str)
-
-  if (!urlObj.hostname && !urlObj.pathname) {
-    throw new Error('Invalid URL')
-  }
-
-  // Remove fragment
-  urlObj.hash = ''
-
-  if (urlObj.pathname) {
-    // Remove duplicate slashes
-    urlObj.pathname = urlObj.pathname.replace(/\/{2,}/g, '/')
-  }
-
-  if (urlObj.hostname) {
-    // Remove trailing dot
-    urlObj.hostname = urlObj.hostname.replace(/\.$/, '')
-  }
-
-  // Remove URL with empty query string
-  if (urlObj.search === '?' || urlObj.search === '') {
-    urlObj.search = ''
-  }
-
-  // Remove query unwanted parameters
-  for (const key in urlObj.searchParams) {
-    if (testParameter(key, REMOVE_QUERY_PARAMS)) {
-      urlObj.searchParams.delete(key)
-    }
-  }
-
-  // Take advantage of many of the Node `url` normalizations
-  str = urlObj.toString()
-
-  // Remove ending `/`
-  str = str.replace(/\/$/, '')
-
-  return str
-}
-
-const getAbsoluteUrl = (baseUrl, relativePath = '') => resolveUrl(baseUrl, relativePath)
+const getAbsoluteUrl = (baseUrl, relativePath = '') => (
+  isRelativeUrl(relativePath)
+    ? resolveUrl(baseUrl, relativePath)
+    : relativePath
+)
 
 const getUrl = (baseUrl, relativePath, opts) => (
   normalizeUrl(getAbsoluteUrl(baseUrl, relativePath), opts)
