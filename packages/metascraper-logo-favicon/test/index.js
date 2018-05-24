8e8b4a9c885ff0550825655febc01e64cde3aeff
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-05-24 17:57:58 +0200

    Update test

--- packages/metascraper-logo-favicon/test/index.js
+++ packages/metascraper-logo-favicon/test/index.js
@@ -23,7 +23,7 @@ const readFile = promisify(fs.readFile)
 
 describe('metascraper-logo-favicon', () => {
   describe('metascraper logo favicon', () => {
-    it('create an absolute faivcon url if the logo is not present', async () => {
+    it('create an absolute favicon url if the logo is not present', async () => {
       const html = await readFile(resolve(__dirname, 'fixtures/input.html'))
       const url = 'https://www.amazon.co.uk/Vegetable-Perfection-tasty-recipes-shoots/dp/1849757097/ref=asap_bc?ie=UTF8'
       const meta = omit(await metascraper({ html, url }), ['date'])
