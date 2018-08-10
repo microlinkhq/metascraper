1a64a149f4f089bc7c114c396d72b4c7435259f4
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-08-10 15:09:52 +0200

    Detect extractors without HTTP request

--- packages/metascraper-video-provider/scripts/postinstall.js
+++ packages/metascraper-video-provider/scripts/postinstall.js
@@ -1,18 +1,21 @@
 'use strict'
 
+const parseDomain = require('parse-domain')
 const jsonFuture = require('json-future')
-const cheerio = require('cheerio')
-const got = require('got')
+const youtubedl = require('youtube-dl')
+const { promisify } = require('util')
+
+const getExtractors = promisify(youtubedl.getExtractors)
 
 ;(async () => {
-  const { body } = await got('https://rg3.github.io/youtube-dl/supportedsites.html')
-  const $ = cheerio.load(body)
-  const set = new Set()
+  const extractors = await getExtractors()
 
-  $('li').each(function (i, el) {
-    const domain = $(el).text().split(':')[0].toLowerCase()
-    set.add(domain)
-  })
+  const providers = extractors.reduce((set, extractor) => {
+    const provider = extractor.split(':')[0].toLowerCase()
+    const { domain = '' } = parseDomain(provider) || {}
+    set.add(domain || provider)
+    return set
+  }, new Set())
 
-  await jsonFuture.saveAsync('providers.json', Array.from(set))
+  await jsonFuture.saveAsync('providers.json', Array.from(providers))
 })()
