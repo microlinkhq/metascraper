9f09429554f9868ae27006f04a15a1a2d5cd714f
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-08-29 10:48:36 +0200

    Add audio support

--- packages/metascraper-media-provider/src/index.js
+++ packages/metascraper-media-provider/src/index.js
@@ -8,6 +8,7 @@ const {
   get,
   chain,
   find,
+  negate,
   isString
 } = require('lodash')
 const { isUrl, titleize } = require('@metascraper/helpers')
@@ -15,40 +16,53 @@ const path = require('path')
 
 const getVideoInfo = require('./get-video-info')
 
-const isMp4 = video =>
-  eq(get(video, 'ext'), 'mp4') ||
-  path.extname(get(video, 'url')).startsWith('.mp4')
-const isHttp = video => eq(get(video, 'protocol'), 'http')
-const isHttps = video => eq(get(video, 'protocol'), 'https')
-const hasAudio = video => has(video, 'abr')
+const isMIME = ext => item =>
+  eq(get(item, 'ext'), ext) ||
+  path.extname(get(item, 'url')).startsWith(`.${ext}`)
 
-const getVideoUrls = (videos, filters = []) => {
+const isMp4 = isMIME('mp4')
+const isMp3 = isMIME('mp3')
+const isM4a = isMIME('m4a')
+const isAac = isMIME('aac')
+
+const isHttp = item => eq(get(item, 'protocol'), 'http')
+const isHttps = item => eq(get(item, 'protocol'), 'https')
+const hasAudio = item => has(item, 'abr')
+const hasVideo = item => has(item, 'tbr')
+
+const getFormatUrls = orderBy => (videos, filters = []) => {
   const urls = chain(videos)
     .filter(overEvery(filters))
-    .orderBy('tbr', 'asc')
+    .orderBy(orderBy, 'asc')
     .map('url')
+    .filter(isUrl)
     .value()
 
   return isEmpty(urls) ? false : urls
 }
 
-const getVideoProvider = async ({ url }) => {
-  const { formats } = await getVideoInfo(url)
+const getVideoUrls = getFormatUrls('tbr')
+const getAudioUrls = getFormatUrls('abr')
 
-  const videoUrl =
-    getVideoUrls(formats, [isMp4, isHttps, hasAudio]) ||
-    getVideoUrls(formats, [isMp4, isHttp, hasAudio]) ||
-    getVideoUrls(formats, [isMp4, isHttps]) ||
-    getVideoUrls(formats, [isMp4])
+const getVideo = ({ formats }) =>
+  getVideoUrls(formats, [hasAudio, isMp4, isHttps]) ||
+  getVideoUrls(formats, [hasAudio, isMp4, isHttp]) ||
+  getVideoUrls(formats, [isMp4, isHttps]) ||
+  getVideoUrls(formats, [isMp4, isHttp]) ||
+  getVideoUrls(formats, [isMp4])
 
-  return isUrl(videoUrl) && videoUrl
-}
+const getAudio = ({ formats }) =>
+  getAudioUrls(formats, [negate(hasVideo), isMp3, isHttps]) ||
+  getAudioUrls(formats, [negate(hasVideo), isAac, isHttps]) ||
+  getAudioUrls(formats, [negate(hasVideo), isM4a, isHttps]) ||
+  getAudioUrls(formats, [negate(hasVideo), isMp3, isHttp]) ||
+  getAudioUrls(formats, [negate(hasVideo), isAac, isHttp]) ||
+  getAudioUrls(formats, [negate(hasVideo), isM4a, isHttp]) ||
+  getAudioUrls(formats, [negate(hasVideo), isMp3]) ||
+  getAudioUrls(formats, [negate(hasVideo), isAac]) ||
+  getAudioUrls(formats, [negate(hasVideo), isM4a])
 
-/**
- * Get the Author of the video.
- */
-const getVideoAuthor = async ({ url }) => {
-  const { uploader, creator, uploader_id: uploaderId } = await getVideoInfo(url)
+const getAuthor = ({ uploader, creator, uploader_id: uploaderId }) => {
   const author = find(
     [creator, uploader, uploaderId],
     str => isString(str) && !isUrl(str, { relative: false })
@@ -56,36 +70,32 @@ const getVideoAuthor = async ({ url }) => {
   return author && titleize(author, { removeBy: true })
 }
 
-const getVideoPublisher = async ({ url }) => {
-  const { extractor_key: extractorKey } = await getVideoInfo(url)
-  return isString(extractorKey) && extractorKey
-}
+const getPublisher = ({ extractor_key: extractorKey }) =>
+  isString(extractorKey) && extractorKey
 
-const getVideoTitle = async ({ url }) => {
-  const { title: mainTitle, alt_title: secondaryTitle } = await getVideoInfo(
-    url
-  )
+const getTitle = ({ title: mainTitle, alt_title: secondaryTitle }) => {
   const title = find([mainTitle, secondaryTitle], isString)
   return title && titleize(title)
 }
 
-const getVideoDate = async ({ url }) => {
-  const { timestamp } = await getVideoInfo(url)
-  return timestamp && new Date(timestamp * 1000).toISOString()
-}
+const getDate = ({ timestamp }) =>
+  timestamp && new Date(timestamp * 1000).toISOString()
 
 module.exports = () => {
   return {
-    video: getVideoProvider,
-    author: getVideoAuthor,
-    publisher: getVideoPublisher,
-    title: getVideoTitle,
-    date: getVideoDate
+    video: async ({ url }) => getVideo(await getVideoInfo(url)),
+    audio: async ({ url }) => getAudio(await getVideoInfo(url)),
+    author: async ({ url }) => getAuthor(await getVideoInfo(url)),
+    publisher: async ({ url }) => getPublisher(await getVideoInfo(url)),
+    title: async ({ url }) => getTitle(await getVideoInfo(url)),
+    date: async ({ url }) => getDate(await getVideoInfo(url))
   }
 }
 
-module.exports.getVideoUrls = getVideoUrls
-module.exports.isMp4 = isMp4
-module.exports.isHttp = isHttp
-module.exports.isHttps = isHttps
-module.exports.hasAudio = hasAudio
+module.exports.getFormatUrls = getFormatUrls
+module.exports.getVideo = getVideo
+module.exports.getAudio = getAudio
+module.exports.getAuthor = getAuthor
+module.exports.getPublisher = getPublisher
+module.exports.getTitle = getTitle
+module.exports.getDate = getDate
