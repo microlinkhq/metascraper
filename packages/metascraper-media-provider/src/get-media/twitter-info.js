82b121802a2e81c49613ce45bc8998210990647d
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-09-27 00:22:25 +0200

    Update rate limit window
    https://developer.twitter.com/en/docs/basics/rate-limiting.html

--- packages/metascraper-media-provider/src/get-media/twitter-info.js
+++ packages/metascraper-media-provider/src/get-media/twitter-info.js
@@ -22,7 +22,7 @@ const isTwitterUrl = url => isTwitterHost(url) && isTweet(url)
 const getTweetId = url => url.split('/').reverse()[0]
 
 const API_GUEST_ACTIVATE_LIMIT = 180
-const API_GUEST_ACTIVATE_EXPIRE = 10 * 60 * 1000 // 10 min
+const API_GUEST_ACTIVATE_EXPIRE = 15 * 60 * 1000 // 15 min
 
 const getGuestToken = async url => {
   const { body } = await got.post(
