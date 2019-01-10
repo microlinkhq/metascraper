80d448718529717a8a27af8c69fda8345c3d3d92
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-01-10 13:21:54 +0100

    test: update snapshots

---
+++ packages/metascraper/test/integration/engadget/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const snapshot = require('snap-shot')
+const { promisify } = require('util')
+const { resolve } = require('path')
+
+const fs = require('fs')
+
+const metascraper = require('../../..')([
+  require('metascraper-author')(),
+  require('metascraper-date')(),
+  require('metascraper-description')(),
+  require('metascraper-video')(),
+  require('metascraper-image')(),
+  require('metascraper-lang')(),
+  require('metascraper-logo')(),
+  require('metascraper-logo-favicon')(),
+  require('metascraper-publisher')(),
+  require('metascraper-title')(),
+  require('metascraper-url')()
+])
+
+const readFile = promisify(fs.readFile)
+
+const url =
+  'https://www.engadget.com/2019/01/07/all-github-users-keep-code-private'
+
+it('engadget', async () => {
+  const html = await readFile(resolve(__dirname, 'input.html'))
+  const metadata = await metascraper({ html, url })
+  snapshot(metadata)
+})
