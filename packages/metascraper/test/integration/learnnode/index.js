c1df0d3d03e052f5354ba2b7969a48362f402eae
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-03-16 20:16:41 +0100

    fix: add author size limitation (#154)
    Just for skipping false-hood values .cc @wooorm

--- packages/metascraper/test/integration/learnnode/index.js
+++ packages/metascraper/test/integration/learnnode/index.js
@@ -22,10 +22,9 @@ const metascraper = require('../../..')([
 
 const readFile = promisify(fs.readFile)
 
-const url =
-  'http://tech.economictimes.indiatimes.com/news/corporate/michael-dell-biggest-tech-acquisition-future-bets/52424992'
+const url = 'https://learnnode.com'
 
-it('et-tech', async () => {
+it('learnnode', async () => {
   const html = await readFile(resolve(__dirname, 'input.html'))
   const metadata = await metascraper({ html, url })
   snapshot(metadata)
