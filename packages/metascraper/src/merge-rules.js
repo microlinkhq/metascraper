8ca826dd3a095b0afa43d69a5eef69e6318686c0
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-04-03 09:41:53 +0200

    feat: add inline rules at the end

--- packages/metascraper/src/merge-rules.js
+++ packages/metascraper/src/merge-rules.js
@@ -2,13 +2,17 @@
 
 const { cloneDeep, concat, first, findIndex, forEach, chain } = require('lodash')
 
-module.exports = (rules, baseRules) => chain(rules)
-  .reduce((acc, rules) => {
-    forEach(rules, (rule, propName) => {
-      const index = findIndex(acc, item => first(item) === propName)
-      if (index !== -1) acc[index][1] = concat(acc[index][1], rule)
-      else acc.push([propName, rule])
-    })
-    return acc
-  }, cloneDeep(baseRules))
-  .value()
+module.exports = (rules, baseRules) =>
+  chain(rules)
+    .reduce((acc, rules) => {
+      forEach(rules, (rule, propName) => {
+        // find the rules associated with `propName`
+        const index = findIndex(acc, item => first(item) === propName)
+        // if `propName` has more rule, add the new rule from the end
+        if (index !== -1) acc[index][1] = concat(rule, ...acc[index][1])
+        // otherwise, create an array of rules
+        else acc.push([propName, rule])
+      })
+      return acc
+    }, cloneDeep(baseRules))
+    .value()
