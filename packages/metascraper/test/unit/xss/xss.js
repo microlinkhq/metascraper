a68be00eb94ec0a7bf3619c23b618c2b434e81a6
David Eriksson <david@activout.se>
2019-04-28 16:02:46 +0200

    fix: use should instead of assert

--- packages/metascraper/test/unit/xss/xss.js
+++ packages/metascraper/test/unit/xss/xss.js
@@ -4,30 +4,25 @@ const path = require('path')
 const metascraper = require(path.join(__dirname, '..', '..', '..'))([
   require(path.join(__dirname, '..', '..', '..', '..', 'metascraper-title'))()
 ])
-const assert = require('assert')
+const should = require('should')
 
 describe('xss', () => {
   // https://github.com/microlinkhq/metascraper/issues/96
   // https://www.npmjs.com/advisories/603
   it('escape attribute value', done => {
-    fs.readFile(
-      path.join(__dirname, 'files', 'issue-96.html'),
-      'utf8',
-      (error, html) => {
-        if (error) {
-          throw error
-        }
-        metascraper({
-          url: 'http://127.0.0.1:8080',
-          html: html
-        }).then(metadata => {
-          assert.equal(
-            metadata.title,
-            '&lt;script src=‘http://127.0.0.1:8080/malware.js’&gt;&lt;/script&gt;'
-          )
-          done()
-        })
+    fs.readFile(path.join(__dirname, 'files', 'issue-96.html'), 'utf8', (error, html) => {
+      if (error) {
+        throw error
       }
-    )
+      metascraper({
+        url: 'http://127.0.0.1:8080',
+        html: html
+      }).then(metadata => {
+        should(metadata.title).be.equal(
+          '&lt;script src=‘http://127.0.0.1:8080/malware.js’&gt;&lt;/script&gt;'
+        )
+        done()
+      })
+    })
   })
 })
