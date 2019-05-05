d6a9f925599e31860376fed9184192045b639688
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-05-05 12:23:04 +0200

    refactor: xss tests

--- packages/metascraper/test/unit/interface.js
+++ packages/metascraper/test/unit/interface.js
@@ -28,6 +28,34 @@ it('url is required', async () => {
   }
 })
 
+it('escape is enabled by default', async () => {
+  const html = `
+  <!doctype html>
+  <html xmlns:og="http://ogp.me/ns#" lang="en">
+
+  <head>
+      <meta charset="utf8">
+      <title>metascraper</title>
+      <meta property="og:description" content="The HR startups go to war.">
+      <meta property="og:image" content="image">
+      <meta property="og:title" content="<script src='http://127.0.0.1:8080/malware.js'></script>">
+      <meta property="og:type" content="article">
+      <meta property="og:url" content="http://127.0.0.1:8080">
+  </head>
+
+  <body>
+  </body>
+  </html>
+  `
+
+  const metadata = await metascraper({
+    html: html,
+    url: 'http://127.0.0.1:8080'
+  })
+
+  should(metadata.title).be.equal('<script src=‘http://127.0.0.1:8080/malware.js’></script>')
+})
+
 it('load extra rules', async () => {
   const url = 'https://microlink.io'
 
---
+++ packages/metascraper/test/unit/xss.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const metascraper = require('metascraper')([require('metascraper-title')()])
+const snapshot = require('snap-shot')
+const should = require('should')
+
+const html = `
+<!doctype html>
+<html xmlns:og="http://ogp.me/ns#" lang="en">
+
+<head>
+    <meta charset="utf8">
+    <title>metascraper</title>
+    <meta property="og:description" content="The HR startups go to war.">
+    <meta property="og:image" content="image">
+    <meta property="og:title" content="<script src='http://127.0.0.1:8080/malware.js'></script>">
+    <meta property="og:type" content="article">
+    <meta property="og:url" content="http://127.0.0.1:8080">
+</head>
+
+<body>
+</body>
+</html>
+`
+
+describe('xss', () => {
+  it('explicitily disable escape', async () => {
+    const metadata = await metascraper({
+      html: html,
+      url: 'http://127.0.0.1:8080',
+      escape: false
+    })
+
+    should(metadata.title).be.equal('<script src=‘http://127.0.0.1:8080/malware.js’></script>')
+  })
+
+  it('escape when the value is not empty', async () => {
+    const metadata = await metascraper({
+      html: html,
+      url: 'http://127.0.0.1:8080',
+      escape: true
+    })
+
+    snapshot(metadata)
+  })
+})
