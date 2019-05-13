80f0f6cca9d18dc9cf18f95a235d53ff0bdbee81
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-05-13 16:40:08 +0200

    build: add real errors

--- packages/metascraper-media-provider/src/get-media/provider/generic.js
+++ packages/metascraper-media-provider/src/get-media/provider/generic.js
@@ -11,17 +11,9 @@ const getInfo = promisify(youtubedl.getInfo)
 
 const REGEX_RATE_LIMIT = /429: Too Many Requests/i
 
-const getFlags = ({ userAgent, cacheDir }) => {
-  const flags = [
-    '--no-warnings',
-    '--no-check-certificate',
-    '--prefer-free-formats',
-    '--youtube-skip-dash-manifest'
-  ]
-  if (cacheDir) flags.push(`--cache-dir=${cacheDir}`)
-  if (userAgent) flags.push(`--user-agent=${userAgent}`)
-  return flags
-}
+const REGEX_ERR_MESSAGE = /ERROR: (.*);?/
+
+const isTwitterRateLimit = (url, err) => isTwitterUrl(url) && REGEX_RATE_LIMIT.test(err.message)
 
 const proxyUri = agent => {
   const { proxy } = agent.options
@@ -29,29 +21,64 @@ const proxyUri = agent => {
   return `https://${proxyAuth}@${host}:${port}`
 }
 
+const getFlags = ({ url, agent, userAgent, cacheDir }) => {
+  const flags = [
+    '--no-warnings',
+    '--no-check-certificate',
+    '--prefer-free-formats',
+    '--youtube-skip-dash-manifest',
+    `--referer=${url}`
+  ]
+  if (cacheDir) flags.push(`--cache-dir=${cacheDir}`)
+  if (userAgent) flags.push(`--user-agent=${userAgent}`)
+  if (agent) flags.push(`--proxy=${proxyUri(agent)}`)
+  return flags
+}
+
+const makeError = ({ rawError, url, flags }) => {
+  let message
+
+  if (rawError.message) {
+    const result = REGEX_ERR_MESSAGE.exec(rawError.message)
+    const [, extractedMessage] = result
+    if (extractedMessage) message = extractedMessage.split('; ')[0]
+  } else {
+    message = rawError
+  }
+
+  const error = new Error(message)
+  error.name = 'youtubedlError'
+  error.code = rawError.code
+  error.signal = rawError.signal
+  error.url = url
+  error.flags = flags
+
+  return error
+}
+
 module.exports = ({ getTunnel, onError, userAgent, cacheDir }) => {
-  const baseFlags = getFlags({ userAgent, cacheDir })
   let retry = expirableCounter()
 
   return async url => {
-    const flags = baseFlags.concat([`--referer=${url}`])
     const tunnel = await getTunnel
+    const agent = isTwitterUrl(url) && retry.val() ? await getAgent({ tunnel }) : undefined
+    const flags = getFlags({ url, agent, userAgent, cacheDir })
     let data = {}
 
     do {
+      debug(`getInfo retry=${retry.val()} url=${url} flags=${flags.join(' ')}`)
       try {
-        const agent = isTwitterUrl(url) && retry.val() ? await getAgent({ tunnel }) : undefined
-        debug(`getInfo retry=${retry.val()} agent=${!!agent} url=${url} flags=${flags.join(' ')}`)
-        data = await getInfo(url, agent ? flags.concat(`--proxy=${proxyUri(agent)}`) : flags)
-      } catch (err) {
+        data = await getInfo(url, flags)
+      } catch (rawError) {
+        const err = makeError({ rawError, url, flags })
         retry.incr()
         onError(err, url)
-        if (!(tunnel && isTwitterUrl(url) && REGEX_RATE_LIMIT.test(err.message))) {
-          return data
-        }
+        if (!(tunnel && isTwitterRateLimit(url, err))) return data
       }
     } while (isEmpty(data))
 
     return data
   }
 }
+
+module.exports.makeError = makeError
