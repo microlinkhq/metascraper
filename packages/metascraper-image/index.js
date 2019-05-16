06220394be3fcb0289f92ebd268777fcb3ac1e0e
Sampo Juustila <sampo.juustila@vincit.fi>
2019-05-16 14:06:44 +0300

    feat: implement JSON-LD retrieval of image

--- packages/metascraper-image/index.js
+++ packages/metascraper-image/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { $filter, url: urlFn } = require('@metascraper/helpers')
+const { $filter, url: urlFn, isString, isArray } = require('@metascraper/helpers')
 
 /**
  * Wrap a rule with validation and formatting logic.
@@ -14,6 +14,17 @@ const wrap = rule => ({ htmlDom, url }) => {
   return urlFn(value, { url })
 }
 
+const findImageFromLd = ({ jsonLd, url }) => {
+  const value = jsonLd.image
+  if (isString(value)) {
+    return urlFn(value, { url })
+  }
+  if (isArray(value) && value.length > 0 && value[0].url) {
+    return urlFn(value[0].url, { url })
+  }
+  return null
+}
+
 const getSrc = el => el.attr('src')
 
 /**
@@ -21,6 +32,7 @@ const getSrc = el => el.attr('src')
  */
 module.exports = () => ({
   image: [
+    findImageFromLd,
     wrap($ => $('meta[property="og:image:secure_url"]').attr('content')),
     wrap($ => $('meta[property="og:image:url"]').attr('content')),
     wrap($ => $('meta[property="og:image"]').attr('content')),
