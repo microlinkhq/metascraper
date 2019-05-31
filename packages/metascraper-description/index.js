91298c183540a7e2db04dc6d6a11c7c40dfc96c8
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-05-31 18:45:29 +0200

    build: move rule
    low priority

--- packages/metascraper-description/index.js
+++ packages/metascraper-description/index.js
@@ -21,11 +21,11 @@ const wrap = rule => ({ htmlDom, url }) => {
 module.exports = () => ({
   description: [
     wrap($jsonld('description')),
-    wrap($jsonld('articleBody')),
     wrap($ => $('meta[property="og:description"]').attr('content')),
     wrap($ => $('meta[name="twitter:description"]').attr('content')),
     wrap($ => $('meta[name="description"]').attr('content')),
     wrap($ => $('meta[itemprop="description"]').attr('content')),
+    wrap($jsonld('articleBody')),
     wrap($ => $('#description').text()),
     wrap($ => $filter($, $('[class*="content" i] > p'))),
     wrap($ => $filter($, $('[class*="content" i] p')))
