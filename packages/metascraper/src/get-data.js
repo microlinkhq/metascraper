73bf56616a7fe31630c843936700e23d316c9d8a
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-05-31 18:47:19 +0200

    build: remove jsonLd variable

--- packages/metascraper/src/get-data.js
+++ packages/metascraper/src/get-data.js
@@ -1,6 +1,14 @@
 'use strict'
 
-const { map, fromPairs, isEmpty, isObject, isArray, mapValues } = require('lodash')
+const {
+  map,
+  fromPairs,
+  isEmpty,
+  isObject,
+  isArray,
+  mapValues
+} = require('lodash')
+
 const xss = require('xss')
 
 const getValue = async ({ htmlDom, jsonLd, url, conditions, meta }) => {
@@ -16,17 +24,24 @@ const getValue = async ({ htmlDom, jsonLd, url, conditions, meta }) => {
 }
 
 const mapValuesDeep = (object, fn) => {
-  if (isArray(object)) return map(object, innerObject => mapValuesDeep(innerObject, fn))
-  if (isObject(object)) return mapValues(object, value => mapValuesDeep(value, fn))
+  if (isArray(object)) {
+    return map(object, innerObject => mapValuesDeep(innerObject, fn))
+  }
+
+  if (isObject(object)) {
+    return mapValues(object, value => mapValuesDeep(value, fn))
+  }
+
   return fn(object)
 }
 
-const escapeValue = (value, { escape }) => (!escape ? value : mapValuesDeep(value, xss))
+const escapeValue = (value, { escape }) =>
+  !escape ? value : mapValuesDeep(value, xss)
 
-const getData = async ({ rules, htmlDom, jsonLd, url, escape }) => {
+const getData = async ({ rules, htmlDom, url, escape }) => {
   const data = await Promise.all(
     map(rules, async ([propName, conditions]) => {
-      const rawValue = await getValue({ htmlDom, url, jsonLd, conditions })
+      const rawValue = await getValue({ htmlDom, url, conditions })
       const value = isEmpty(rawValue) ? null : escapeValue(rawValue, { escape })
       return [propName, value]
     })
