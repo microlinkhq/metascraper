fcfcb64d7bea6628e06aa1d5b021d9e08a119aaf
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-06-20 15:03:58 +0200

    feat: add metascraper-uol

---
+++ packages/metascraper-uol/index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const { $jsonld, title, description } = require('@metascraper/helpers')
+const memoizeOne = require('memoize-one')
+const { getDomain } = require('tldts')
+
+const ROOT_DOMAINS = ['uol.com.br', 'torcedores.com']
+
+const isValidUrl = memoizeOne(url =>
+  ROOT_DOMAINS.some(domain => getDomain(url) === domain)
+)
+
+const ward = fn => args => (isValidUrl(args.url) ? fn(args) : null)
+
+const createWrapper = fn => rule => ({ htmlDom, url }) => {
+  const value = rule(htmlDom, url)
+  return fn(value)
+}
+
+const wrapTitle = createWrapper(title)
+const wrapDescription = createWrapper(description)
+
+module.exports = () => {
+  return {
+    title: [
+      ward(wrapTitle(($, url) => $jsonld('headline')($, url))),
+      ward(wrapTitle($ => $('title').text()))
+    ],
+    description: [
+      ward(wrapDescription(($, url) => $jsonld('description')($, url)))
+    ]
+  }
+}
+
+module.exports.isValidUrl = isValidUrl
