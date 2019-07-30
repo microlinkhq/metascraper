70c9e8eec6c4e06678f794ced3734d1e8b72d49f
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-07-30 16:58:13 +0200

    refactor: better interface

--- packages/metascraper-clearbit/README.md
+++ packages/metascraper-clearbit/README.md
@@ -13,38 +13,27 @@ $ npm install metascraper-clearbit --save
 
 ## API
 
-### clearbitLogo([options])
+### netascraper-clearbit([options])
 
 #### options
 
-##### size
+#### gotOpts
 
-Type: `string`<br>
-Default: `128`
+Any option provided here will passed to [got#options](https://github.com/sindresorhus/got#options).
 
-Length of longest side in pixels.
+In addition, these options are set by default:
 
-##### format
+```json
+{
+  "retry": 0,
+  "json": true,
+  "timeout": 1500
+}
+```
 
-Type: `string`<br>
-Default: `png`
+#### logoOpts
 
-Image format, either "png" or "jpg".
-
-##### greyscale
-
-Type: `boolean`<br>
-Default: `false`
-
-Desaturates image if set to `true`.
-
-##### timeout
-
-Type: `number`<br>
-Default: 1000
-
-Time in milliseconds before giving up on the Clearbit API request and
-returning `null`.
+Any option provided here will passed to [Clearbit Logo API](https://clearbit.com/docs#logo-api).
 
 ## License
 
--- packages/metascraper-clearbit/test/index.js
+++ packages/metascraper-clearbit/test/index.js
@@ -2,23 +2,50 @@
 
 const snapshot = require('snap-shot')
 
-const metascraper = require('metascraper')([require('..')()])
+const createMetascraperClearbit = require('..')
+const createMetascraper = require('metascraper')
 
 describe('metascraper-clearbit', () => {
-  it('returns when is possible resolve logo', async () => {
-    const url = 'https://facebook.com'
+  it('returns returns logo url if it exists', async () => {
+    const metascraper = createMetascraper([createMetascraperClearbit()])
+    const url = 'https://microlink.io'
     const meta = await metascraper({ url })
     snapshot(meta)
   })
 
-  it('returns null if timeout is passed', async () => {
-    const url = 'https://facebook.com'
-    const meta = await metascraper({ url, timeout: 1 })
+  it('returns null if no logo available', async () => {
+    const metascraper = createMetascraper([createMetascraperClearbit()])
+    const url = 'https://lolwerhere.com'
+    const meta = await metascraper({ url })
     snapshot(meta)
   })
 
-  it('returns null if no logo available', async () => {
-    const url = 'https://lolwerhere.com'
+  it('accept `gotOpts` options', async () => {
+    const metascraper = createMetascraper([
+      createMetascraperClearbit({
+        gotOpts: {
+          timeout: 1
+        }
+      })
+    ])
+    const url = 'https://microlink.io'
+    const meta = await metascraper({ url })
+    snapshot(meta)
+  })
+
+  it('accept `logoOpts` options', async () => {
+    const metascraper = createMetascraper([
+      createMetascraperClearbit({
+        gotOpts: {
+          timeout: 5000
+        },
+        logoOpts: {
+          format: 'jpg',
+          greyscale: true
+        }
+      })
+    ])
+    const url = 'https://microlink.io'
     const meta = await metascraper({ url })
     snapshot(meta)
   })
