78ac024abe9060f32b20f0e916d7c48d6f23463b
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-07-31 23:06:56 +0200

    test: update snapshot

--- packages/metascraper-logo-favicon/test/index.js
+++ packages/metascraper-logo-favicon/test/index.js
@@ -1,9 +1,8 @@
 'use strict'
 
-const snapshot = require('snap-shot')
 const { promisify } = require('util')
 const { resolve } = require('path')
-const { omit } = require('lodash')
+const should = require('should')
 const fs = require('fs')
 
 const metascraper = require('metascraper')([require('..')()])
@@ -13,9 +12,8 @@ const readFile = promisify(fs.readFile)
 describe('metascraper-logo-favicon', () => {
   it('create an absolute favicon url if the logo is not present', async () => {
     const html = await readFile(resolve(__dirname, 'fixtures/input.html'))
-    const url =
-      'https://www.amazon.co.uk/Vegetable-Perfection-tasty-recipes-shoots/dp/1849757097/ref=asap_bc?ie=UTF8'
-    const meta = omit(await metascraper({ html, url }), ['date'])
-    snapshot(meta)
+    const url = 'https://microlink.io'
+    const meta = await metascraper({ url, html })
+    should(meta.logo).be.equal('https://microlink.io/favicon.ico')
   })
 })
