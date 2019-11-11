74d90695e6fb03bd9f19385483c9c227e199402c
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-12 00:01:09 +0100

    build: ensure youtube id is present

--- packages/metascraper-iframe/src/providers/youtube.js
+++ packages/metascraper-iframe/src/providers/youtube.js
@@ -2,13 +2,14 @@
 
 const getYouTubeID = require('get-youtube-id')
 const youtubeRegex = require('youtube-regex')
-const { stringify } = require('querystring')
 
-const youtube = ({ url, height = 360, width = 480, ...query }) =>
-  `<iframe src="https://www.youtube.com/embed/${getYouTubeID(url)}?${stringify(
+const toQuery = require('../to-query')
+
+const youtube = ({ url, height = 315, width = 560, ...query }) =>
+  `<iframe src="https://www.youtube.com/embed/${getYouTubeID(url)}${toQuery(
     query
   )}" width="${width}" height="${height}" frameborder="0" allowfullscreen>`
 
-youtube.test = url => youtubeRegex().test(url)
+youtube.test = url => youtubeRegex().test(url) && getYouTubeID(url)
 
 module.exports = youtube
--- packages/metascraper-iframe/test/youtube.js
+++ packages/metascraper-iframe/test/youtube.js
@@ -8,18 +8,14 @@ const createMetascraper = require('metascraper')
 describe('metascraper-iframe » youtube', () => {
   describe('test urls', () => {
     ;[
-      'https://www.youtube.com/watch?feature=player_embedded&v=21Lk4YiASMo',
-      'https://youtu.be/21Lk4YiASMo',
-      'https://www.youtube.com/channel/UCL8ZULXASCc1I_oaOT0NaOQ',
-      'http://www.youtube.com/user/googlechrome',
-      'https://www.youtube.com/playlist?list=PL5308B2E5749D1696',
-      'https://www.youtube.com/oembed?format=json&url=https://www.youtube.com/watch?v=21Lk4YiASMo',
-      'https://www.youtube.com/oembed?format=json&url=https://www.youtube.com/playlist?list=PL5308B2E5749D1696'
+      'https://www.youtube.com/watch?v=Gu8X7vM3Avw',
+      'https://youtu.be/Gu8X7vM3Avw',
+      'https://www.youtube.com/watch?v=-TWztwbOpog&list=PL5aqr5w5fRe4nO30px44D5sBukIUw1UwX'
     ].forEach(url => {
       it(url, async () => {
         const metascraper = createMetascraper([createMetascraperIframe()])
         const meta = await metascraper({ url, escape: false })
-        should(meta.iframe).be.not.undefined()
+        should(meta.iframe).be.not.null()
       })
     })
   })
