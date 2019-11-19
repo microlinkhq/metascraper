b6fcc0b6622e44d33aae89465afe97498e891e98
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-19 11:50:58 +0100

    [metascraper-iframe]: Add oembed markup detection (#240)
    * refactor: use fs.promises
    
    * fix: linter
    
    * fix: missing require
    
    * feat: oembed markup detection

--- packages/metascraper-iframe/src/index.js
+++ packages/metascraper-iframe/src/index.js
@@ -2,20 +2,37 @@
 
 const { extract, hasProvider } = require('oembed-parser')
 const { memoizeOne } = require('@metascraper/helpers')
+const { forEach, get } = require('lodash')
+const pReflect = require('p-reflect')
+const got = require('got')
 
-const iframe = async ({ url, meta, htmlDom, ...opts }) => {
-  try {
-    const oembed = await extract(url, opts)
-    return oembed.html
-  } catch (err) {
-    return null
-  }
+const jsonOembed = memoizeOne($ => {
+  const el = $('link[type="application/json+oembed"]')
+  return el.attr('href')
+})
+
+const fromProvider = async ({ url, meta, htmlDom, ...opts }) => {
+  const { value } = await pReflect(extract(url, opts))
+  return get(value, 'html', null)
 }
 
-const isValidUrl = memoizeOne(({ url }) => hasProvider(url))
+const fromHTML = async ({ url, meta, htmlDom, ...opts }) => {
+  const oembedUrl = jsonOembed(htmlDom)
+  if (!oembedUrl) return null
+
+  const oembedUrlObj = new URL(oembedUrl)
+  forEach(opts, (value, key) => oembedUrlObj.searchParams.append(key, value))
+
+  const { value } = await pReflect(got(oembedUrlObj.toString(), { json: true }))
+  return get(value, 'body.html', null)
+}
+
+const isValidUrl = memoizeOne(
+  ({ url, htmlDom: $ }) => !!jsonOembed($) || hasProvider(url)
+)
 
 module.exports = () => {
-  const rules = { iframe }
+  const rules = { iframe: [fromHTML, fromProvider] }
   rules.test = isValidUrl
   return rules
 }
