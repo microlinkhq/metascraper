efd27b53e582ff62deaa22c8b4e0ffa6b8664bc8
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-08 20:23:27 +0200

    test: update url

--- packages/metascraper-iframe/test/index.js
+++ packages/metascraper-iframe/test/index.js
@@ -15,7 +15,7 @@ const commonProviders = [
   'https://youtu.be/Gu8X7vM3Avw',
   'https://www.youtube.com/watch?v=-TWztwbOpog&list=PL5aqr5w5fRe4nO30px44D5sBukIUw1UwX',
   'https://open.spotify.com/track/63W11KVHDOpSlh3XMQ7qMg?si=Yd-hIkD9TtSUeFeR0jzKsA',
-  'https://open.spotify.com/playlist/1xY6msLHX1W34EzB0UkkbU?si=cFF7LjzgQxWz5ni6TCN_jA',
+  'https://open.spotify.com/album/5ht7ItJgpBH7W6vJ5BqpPr',
   'https://vimeo.com/channels/staffpicks/135373919',
   'https://vimeo.com/135373919'
 ]
