1fc53062623a7ce5bdaa5daaa20a809d9c751374
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-02-04 16:32:02 +0100

    test: add integration tests

---
+++ packages/metascraper/test/integration/indiehackers/index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const snapshot = require('snap-shot')
+const { resolve } = require('path')
+const { readFile } = require('fs').promises
+
+const metascraper = require('../../..')([
+  require('metascraper-author')(),
+  require('metascraper-date')(),
+  require('metascraper-description')(),
+  require('metascraper-video')(),
+  require('metascraper-image')(),
+  require('metascraper-lang')(),
+  require('metascraper-logo')(),
+  require('metascraper-logo-favicon')(),
+  require('metascraper-publisher')(),
+  require('metascraper-title')(),
+  require('metascraper-url')(),
+  require('metascraper-readability')(),
+  require('metascraper-audio')()
+])
+
+const url =
+  'https://www.indiehackers.com/podcast/032-peter-and-calvin-of-segment'
+
+it('indiehackers', async () => {
+  const html = await readFile(resolve(__dirname, 'input.html'))
+  const metadata = await metascraper({ html, url })
+  snapshot(metadata)
+})
