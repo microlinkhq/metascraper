8266df08be8800883095adb5cb40ad97a928d7a3
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-02-05 10:13:16 +0100

    test: add integration

---
+++ packages/metascraper/test/integration/simplecast/index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const snapshot = require('snap-shot')
+const { resolve } = require('path')
+const { readFile } = require('fs').promises
+
+const metascraper = require('../../..')([
+  require('metascraper-author')(),
+  require('metascraper-date')(),
+  require('metascraper-description')(),
+  require('metascraper-video')(),
+  require('metascraper-image')(),
+  require('metascraper-lang')(),
+  require('metascraper-logo')(),
+  require('metascraper-logo-favicon')(),
+  require('metascraper-publisher')(),
+  require('metascraper-title')(),
+  require('metascraper-url')(),
+  require('metascraper-readability')(),
+  require('metascraper-audio')()
+])
+
+const url =
+  'https://player.simplecast.com/3ab2ec74-8d89-4344-9be0-a30fa53ac6a7?dark=false'
+
+it('simplecast', async () => {
+  const html = await readFile(resolve(__dirname, 'input.html'))
+  const metadata = await metascraper({ html, url })
+  snapshot(metadata)
+})
