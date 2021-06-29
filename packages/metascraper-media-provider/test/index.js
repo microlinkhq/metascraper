583107d5f62f0276ab590c6dcee264ee1900557b
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-06-29 18:20:59 +0200

    test: update proxy setup (#429)
    * build: remove --no-clean flag
    
    * test: update proxy
    
    * test: use a different facebook video url
    
    * build: force

--- packages/metascraper-media-provider/test/index.js
+++ packages/metascraper-media-provider/test/index.js
@@ -2,25 +2,32 @@
 
 const debug = require('debug-logfmt')('metascraper-media-provider:test')
 const { getDomainWithoutSuffix } = require('tldts')
-const parseProxy = require('parse-proxy-uri')
+const parseProxyUri = require('parse-proxy-uri')
 const snapshot = require('snap-shot')
 const should = require('should')
 const isCI = require('is-ci')
 
-const proxy = parseProxy(process.env.PROXY_URI)
+const { PROXY_PASSWORD, PROXY_USERNAME, PROXY_HOST } = process.env
 
-const PROXY_DOMAINS = ['vimeo']
+const proxy =
+  PROXY_PASSWORD && PROXY_USERNAME && PROXY_HOST
+    ? parseProxyUri(
+        `socks5://${PROXY_USERNAME}:${PROXY_PASSWORD}@${PROXY_HOST}`
+      )
+    : undefined
+
+const PROXY_DOMAINS = ['vimeo', 'facebook']
 const PROXY_URLS = ['https://api.twitter.com/1.1/guest/activate.json']
 
-const getProxy = ({ url, retryCount = 1 }) => {
-  if (retryCount === 0) return false
+const getProxy = ({ url, retryCount }) => {
+  if (retryCount !== 1) return false
   if (PROXY_URLS.includes(url)) return proxy
   if (PROXY_DOMAINS.includes(getDomainWithoutSuffix(url))) return proxy
   return false
 }
 
 const metascraper = require('metascraper')([
-  require('..')({ getProxy, timeout: 10000 }),
+  require('..')({ getProxy, timeout: 15000 }),
   require('metascraper-publisher')(),
   require('metascraper-author')(),
   require('metascraper-date')(),
@@ -68,7 +75,7 @@ describe('metascraper-media-provider', () => {
         })
       })
     })
-    ;(isCI ? describe.skip : describe)('vimeo', () => {
+    ;(isCI ? describe.skip : describe.only)('vimeo', () => {
       ;[
         // TODO: uncomment when the issue is resolved
         // https://github.com/ytdl-org/youtube-dl/issues/29205
@@ -121,10 +128,9 @@ describe('metascraper-media-provider', () => {
         should(isUrl(metadata.video)).be.false()
       })
     })
-
-    describe('facebook', () => {
+    ;(isCI ? describe.skip : describe)('facebook', () => {
       ;[
-        'https://www.facebook.com/afcajax/videos/1686831701364171',
+        'https://www.facebook.com/natgeo/videos/10156364216738951/',
         'https://www.facebook.com/cnn/videos/10157803903591509/'
       ].forEach(url => {
         it(url, async () => {
@@ -146,8 +152,7 @@ describe('metascraper-media-provider', () => {
         })
       })
     })
-
-    describe('facebook', () => {
+    ;(isCI ? describe.skip : describe)('facebook', () => {
       ;[
         'https://www.facebook.com/afcajax/videos/1686831701364171',
         'https://www.facebook.com/cnn/videos/10157803903591509/'
