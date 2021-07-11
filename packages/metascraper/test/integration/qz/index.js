e2485f8e3cdadeced348d2995c94bb5bf28dbded
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-07-11 12:51:10 +0200

    test(integration): add qz.com

---
+++ packages/metascraper/test/integration/qz/index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const snapshot = require('snap-shot')
+const { resolve } = require('path')
+const { omit } = require('lodash')
+const { readFile } = require('fs').promises
+
+const metascraper = require('../../..')([
+  require('metascraper-author')(),
+  require('metascraper-date')(),
+  require('metascraper-description')(),
+  require('metascraper-video')(),
+  require('metascraper-image')(),
+  require('metascraper-lang')(),
+  require('metascraper-logo')(),
+  require('metascraper-logo-favicon')(),
+  require('metascraper-publisher')(),
+  require('metascraper-title')(),
+  require('metascraper-url')(),
+  require('metascraper-readability')(),
+  require('metascraper-audio')()
+])
+
+const url = 'https://qz.com/on/beyond-silicon-valley/'
+
+it('qz', async () => {
+  const html = await readFile(resolve(__dirname, 'input.html'))
+  const metadata = await metascraper({ html, url })
+  snapshot(omit(metadata, ['date']))
+})
