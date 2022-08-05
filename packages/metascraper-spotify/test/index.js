9d04e41b430ae274f28832a0068a7e97708d6b3b
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-08-05 10:15:52 +0200

    refactor(spotify): get descrpition & author from markup

--- packages/metascraper-spotify/test/index.js
+++ packages/metascraper-spotify/test/index.js
@@ -1,6 +1,8 @@
 'use strict'
 
+const { readFile } = require('fs').promises
 const { mapValues } = require('lodash')
+const { resolve } = require('path')
 const kindOf = require('kind-of')
 const test = require('ava')
 
@@ -46,4 +48,22 @@ spotifyUrls.forEach(url => {
     const metadata = await metascraper({ url })
     t.snapshot(mapValues(metadata, kindOf))
   })
+})
+
+test('get `description` from HTML markup', async t => {
+  const url = 'https://open.spotify.com/episode/64TORH3xleuD1wcnFsrH1E'
+  const html = await readFile(resolve(__dirname, 'fixtures/episode.html'))
+
+  const metascraper = createMetascraper()
+  const metadata = await metascraper({ url, html })
+  t.snapshot(metadata)
+})
+
+test('get `author` from HTML markup', async t => {
+  const url = 'https://open.spotify.com/show/4kYCRYJ3yK5DQbP5tbfZby'
+  const html = await readFile(resolve(__dirname, 'fixtures/show.html'))
+
+  const metascraper = createMetascraper()
+  const metadata = await metascraper({ url, html })
+  t.snapshot(metadata)
 })
