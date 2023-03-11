d219b58015551cf5a051942e410fb840a189738d
Stiliyan Ivanov <stily.c@gmail.com>
2023-03-11 18:26:38 +0200

    feat(date): add `datePublished` and `datemodified` support (#374)
    * Add published and modified dates
    
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>
    
    * Fix more tests
    
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>
    
    * Merge code
    
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>
    
    * Fxi modified date
    
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>
    
    * New line
    
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>
    
    * Wee need concat rules
    
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>
    
    * Closer to old behaviour of date
    
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>
    
    * YouTube does not seem to use <meta date>
    
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>
    
    * Add date docs
    
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>
    
    * Update README.md
    
    * test: don't modify previous fixtures
    
    * test: update snapshot
    
    The rule:
    
    ```
    toDate($ => $filter($, $('[class*="publish" i]')))
    ```
    
    is taking more priority than:
    
    ```
    toDate($ => $('[property*="dc:date" i]').attr('content'))
    ```
    
    It's okay for this case since it's a fuzzy rule so isn't too deterministic
    
    * test: update snapshot
    
    The rule
    
    ```
    toDate($ => $('[itemprop="datepublished" i]').attr('content'))
    ```
    
    It's taking more priority than:
    
    ```
    toDate($ => $('meta[name="date" i]').attr('content')
    ```
    
    It's okay for this case since it's a fuzzy rule so isn't too deterministic
    
    * test: update snapshot
    
    ---------
    
    Signed-off-by: Stiliyan Ivanov <stily.c@gmail.com>
    Co-authored-by: Kiko Beats <josefrancisco.verdu@gmail.com>

--- packages/metascraper-date/index.js
+++ packages/metascraper-date/index.js
@@ -4,36 +4,21 @@ const { date, $filter, $jsonld, toRule } = require('@metascraper/helpers')
 
 const toDate = toRule(date)
 
-module.exports = () => ({
-  date: [
-    toDate($jsonld('dateModified')),
-    toDate($jsonld('datePublished')),
-    toDate($jsonld('dateCreated')),
-    toDate($ => $('meta[property*="updated_time" i]').attr('content')),
-    toDate($ => $('meta[property*="modified_time" i]').attr('content')),
-    toDate($ => $('meta[property*="published_time" i]').attr('content')),
-    toDate($ => $('meta[property*="release_date" i]').attr('content')),
+const dateRules = () => {
+  return [
     toDate($ => $('meta[name="date" i]').attr('content')),
-    toDate($ => $('[itemprop*="datemodified" i]').attr('content')),
-    toDate($ => $('[itemprop="datepublished" i]').attr('content')),
     toDate($ => $('[itemprop*="date" i]').attr('content')),
     toDate($ => $('time[itemprop*="date" i]').attr('datetime')),
     toDate($ => $('time[datetime]').attr('datetime')),
-    toDate($ => $('time[datetime][pubdate]').attr('datetime')),
     toDate($ => $('meta[name*="dc.date" i]').attr('content')),
-    toDate($ => $('meta[name*="dc.date.issued" i]').attr('content')),
-    toDate($ => $('meta[name*="dc.date.created" i]').attr('content')),
     toDate($ => $('meta[name*="dcterms.date" i]').attr('content')),
     toDate($ => $('[property*="dc:date" i]').attr('content')),
-    toDate($ => $('[property*="dc:created" i]').attr('content')),
     toDate($ => $filter($, $('[class*="byline" i]'))),
     toDate($ => $filter($, $('[class*="dateline" i]'))),
     toDate($ => $filter($, $('[id*="metadata" i]'))),
     toDate($ => $filter($, $('[class*="metadata" i]'))), // twitter, move into a bundle of rules
     toDate($ => $filter($, $('[id*="date" i]'))),
     toDate($ => $filter($, $('[class*="date" i]'))),
-    toDate($ => $filter($, $('[id*="publish" i]'))),
-    toDate($ => $filter($, $('[class*="publish" i]'))),
     toDate($ => $filter($, $('[id*="post-timestamp" i]'))),
     toDate($ => $filter($, $('[class*="post-timestamp" i]'))),
     toDate($ => $filter($, $('[id*="post-meta" i]'))),
@@ -41,4 +26,45 @@ module.exports = () => ({
     toDate($ => $filter($, $('[id*="time" i]'))),
     toDate($ => $filter($, $('[class*="time" i]')))
   ]
-})
+}
+
+const datePublishedRules = () => {
+  return [
+    toDate($jsonld('datePublished')),
+    toDate($jsonld('dateCreated')),
+    toDate($ => $('meta[property*="published_time" i]').attr('content')),
+    toDate($ => $('meta[property*="release_date" i]').attr('content')),
+    toDate($ => $('[itemprop="datepublished" i]').attr('content')),
+    toDate($ => $('time[datetime][pubdate]').attr('datetime')),
+    toDate($ => $('meta[name*="dc.date.issued" i]').attr('content')),
+    toDate($ => $('meta[name*="dc.date.created" i]').attr('content')),
+    toDate($ => $('[property*="dc:created" i]').attr('content')),
+    toDate($ => $filter($, $('[id*="publish" i]'))),
+    toDate($ => $filter($, $('[class*="publish" i]')))
+  ]
+}
+
+const dateModifiedRules = () => {
+  return [
+    toDate($jsonld('dateModified')),
+    toDate($ => $('meta[property*="updated_time" i]').attr('content')),
+    toDate($ => $('meta[property*="modified_time" i]').attr('content')),
+    toDate($ => $('[itemprop*="datemodified" i]').attr('content'))
+  ]
+}
+
+module.exports = ({ datePublished, dateModified } = { datePublished: false, dateModified: false }) => {
+  const result = {
+    date: dateModifiedRules().concat(datePublishedRules(), dateRules())
+  }
+
+  if (datePublished) {
+    result.datePublished = datePublishedRules()
+  }
+
+  if (dateModified) {
+    result.dateModified = dateModifiedRules()
+  }
+
+  return result
+}
