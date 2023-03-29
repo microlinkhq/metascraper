9aa47c5fcd839d7dd36219af61353a058bf54a9b
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-03-29 11:28:10 +0200

    refactor(date): remove unused rules (#625)
    * refactor(date): remove unused rules
    
    * refactor(date): remove updated_time rule
    
    That rule is just supporting wrong HTML markup
    
    Related: https://twitter.com/Kikobeats/status/1637893039883427859

--- packages/metascraper-date/index.js
+++ packages/metascraper-date/index.js
@@ -10,20 +10,9 @@ const dateRules = () => {
     toDate($ => $('[itemprop*="date" i]').attr('content')),
     toDate($ => $('time[itemprop*="date" i]').attr('datetime')),
     toDate($ => $('time[datetime]').attr('datetime')),
-    toDate($ => $('meta[name*="dc.date" i]').attr('content')),
-    toDate($ => $('meta[name*="dcterms.date" i]').attr('content')),
-    toDate($ => $('[property*="dc:date" i]').attr('content')),
     toDate($ => $filter($, $('[class*="byline" i]'))),
-    toDate($ => $filter($, $('[class*="dateline" i]'))),
-    toDate($ => $filter($, $('[id*="metadata" i]'))),
-    toDate($ => $filter($, $('[class*="metadata" i]'))), // twitter, move into a bundle of rules
     toDate($ => $filter($, $('[id*="date" i]'))),
     toDate($ => $filter($, $('[class*="date" i]'))),
-    toDate($ => $filter($, $('[id*="post-timestamp" i]'))),
-    toDate($ => $filter($, $('[class*="post-timestamp" i]'))),
-    toDate($ => $filter($, $('[id*="post-meta" i]'))),
-    toDate($ => $filter($, $('[class*="post-meta" i]'))),
-    toDate($ => $filter($, $('[id*="time" i]'))),
     toDate($ => $filter($, $('[class*="time" i]')))
   ]
 }
@@ -33,13 +22,7 @@ const datePublishedRules = () => {
     toDate($jsonld('datePublished')),
     toDate($jsonld('dateCreated')),
     toDate($ => $('meta[property*="published_time" i]').attr('content')),
-    toDate($ => $('meta[property*="release_date" i]').attr('content')),
     toDate($ => $('[itemprop="datepublished" i]').attr('content')),
-    toDate($ => $('time[datetime][pubdate]').attr('datetime')),
-    toDate($ => $('meta[name*="dc.date.issued" i]').attr('content')),
-    toDate($ => $('meta[name*="dc.date.created" i]').attr('content')),
-    toDate($ => $('[property*="dc:created" i]').attr('content')),
-    toDate($ => $filter($, $('[id*="publish" i]'))),
     toDate($ => $filter($, $('[class*="publish" i]')))
   ]
 }
@@ -47,13 +30,17 @@ const datePublishedRules = () => {
 const dateModifiedRules = () => {
   return [
     toDate($jsonld('dateModified')),
-    toDate($ => $('meta[property*="updated_time" i]').attr('content')),
     toDate($ => $('meta[property*="modified_time" i]').attr('content')),
     toDate($ => $('[itemprop*="datemodified" i]').attr('content'))
   ]
 }
 
-module.exports = ({ datePublished, dateModified } = { datePublished: false, dateModified: false }) => {
+module.exports = (
+  { datePublished, dateModified } = {
+    datePublished: false,
+    dateModified: false
+  }
+) => {
   const result = {
     date: dateModifiedRules().concat(datePublishedRules(), dateRules())
   }
