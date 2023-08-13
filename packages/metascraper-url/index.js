5749782f4e7cbf5a025ba56e9570c4d4408163de
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-08-13 23:13:04 +0200

    chore(url): remove twitter rules

--- packages/metascraper-url/index.js
+++ packages/metascraper-url/index.js
@@ -7,8 +7,6 @@ const toUrl = toRule(urlFn)
 module.exports = () => ({
   url: [
     toUrl($ => $('meta[property="og:url"]').attr('content')),
-    toUrl($ => $('meta[name="twitter:url"]').attr('content')),
-    toUrl($ => $('meta[property="twitter:url"]').attr('content')),
     toUrl($ => $('link[rel="canonical"]').attr('href')),
     toUrl($ => $('link[rel="alternate"][hreflang="x-default"]').attr('href')),
     ({ url }) => url
