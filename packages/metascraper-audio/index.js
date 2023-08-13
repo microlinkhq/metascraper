cd322eddbb8b684d1ba77aa80dd22cc9f7927e58
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-08-13 20:57:45 +0000

    chore(audio): remove twitter rules

--- packages/metascraper-audio/index.js
+++ packages/metascraper-audio/index.js
@@ -6,20 +6,14 @@ const {
   audio,
   findRule,
   has,
-  isMime,
   loadIframe,
   normalizeUrl,
   toRule
 } = require('@metascraper/helpers')
 
-const memoize = require('@keyvhq/memoize')
 const pReflect = require('p-reflect')
-const got = require('got')
 
 const toAudio = toRule(audio)
-
-const withContentType = (url, contentType) =>
-  isMime(contentType, 'audio') ? url : false
 
 const audioRules = [
   ({ url, htmlDom: $ }) => {
@@ -35,17 +29,6 @@ const audioRules = [
       })
       : undefined
   },
-  toAudio($ => {
-    const contentType =
-      $('meta[name="twitter:player:stream:content_type"]').attr('content') ||
-      $('meta[property="twitter:player:stream:content_type"]').attr('content')
-
-    const streamUrl =
-      $('meta[name="twitter:player:stream"]').attr('content') ||
-      $('meta[property="twitter:player:stream"]').attr('content')
-
-    return contentType ? withContentType(streamUrl, contentType) : streamUrl
-  }),
   toAudio($jsonld('contentUrl')),
   toAudio($ => $('audio').attr('src')),
   toAudio($ => $('audio > source').attr('src')),
@@ -55,22 +38,7 @@ const audioRules = [
 const _getIframe = (url, $, { src }) =>
   loadIframe(url, $.load(`<iframe src="${src}"></iframe>`))
 
-const createGetPlayer = ({ gotOpts, keyvOpts }) => {
-  const getPlayer = async playerUrl => {
-    const { value: response } = await pReflect(got(playerUrl, gotOpts))
-    if (!response) return
-    const contentType = response.headers['content-type']
-    if (!contentType || !contentType.startsWith('text')) return
-    return response.body
-  }
-  return memoize(getPlayer, keyvOpts, {
-    value: value => (value === undefined ? null : value)
-  })
-}
-
-module.exports = ({ getIframe = _getIframe, gotOpts, keyvOpts } = {}) => {
-  const getPlayer = createGetPlayer({ gotOpts, keyvOpts })
-
+module.exports = ({ getIframe = _getIframe } = {}) => {
   return {
     audio: [
       ...audioRules,
@@ -106,18 +74,6 @@ module.exports = ({ getIframe = _getIframe, gotOpts, keyvOpts } = {}) => {
         )
 
         return value
-      },
-      async ({ htmlDom: $, url }) => {
-        const playerUrl =
-          $('meta[name="twitter:player"]').attr('content') ||
-          $('meta[property="twitter:player"]').attr('content')
-        if (!playerUrl) return
-
-        const html = await getPlayer(normalizeUrl(url, playerUrl))
-        if (!html) return
-
-        const htmlDom = $.load(html)
-        return findRule(audioRules, { htmlDom, url })
       }
     ]
   }
--- packages/metascraper-audio/test/index.js
+++ packages/metascraper-audio/test/index.js
@@ -5,31 +5,6 @@ const test = require('ava')
 const createMetascraper = (...args) =>
   require('metascraper')([require('..')(...args)])
 
-test('provide `keyvOpts`', async t => {
-  const cache = new Map()
-  const url = 'https://twitter-card-player.vercel.app'
-  const metascraper = createMetascraper({
-    gotOpts: { retry: 0 },
-    keyvOpts: { store: cache }
-  })
-
-  const metadataOne = await metascraper({
-    url,
-    html: '<meta property="twitter:player" content="https://twitter-card-player.vercel.app/container/audio.html">'
-  })
-
-  t.truthy(metadataOne.audio)
-  t.is(cache.size, 1)
-
-  const metadataTwo = await metascraper({
-    url,
-    html: '<meta property="twitter:player" content="https://twitter-card-player.vercel.app/audio-fail.html">'
-  })
-
-  t.falsy(metadataTwo.audio)
-  t.is(cache.size, 2)
-})
-
 test('og:audio', async t => {
   const html =
     '<meta property="og:audio" content="https://cdn.microlink.io/file-examples/sample.mp3">'
@@ -55,24 +30,6 @@ test('og:audio:secure_url', async t => {
   const metascraper = createMetascraper()
   const metadata = await metascraper({ html, url })
   t.is(metadata.audio, 'https://cdn.microlink.io/file-examples/sample.mp3')
-})
-
-test('twitter:player', async t => {
-  const html =
-    '<meta property="twitter:player" content="https://twitter-card-player.vercel.app/container/audio.html">'
-  const url = 'https://twitter-card-player.vercel.app'
-  const metascraper = createMetascraper()
-  const metadata = await metascraper({ html, url })
-  t.snapshot(metadata)
-})
-
-test('twitter:player:stream', async t => {
-  const html =
-    '<meta property="twitter:player:stream" content="https://cdn.microlink.io/file-examples/sample.mp3">'
-  const url = 'https://twitter-card-player.vercel.app'
-  const metascraper = createMetascraper()
-  const metadata = await metascraper({ html, url })
-  t.snapshot(metadata)
 })
 
 test('audio:src', async t => {
