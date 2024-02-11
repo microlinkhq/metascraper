ac9fc4af23bb945bd6ed6862ffbe44b8784157a6
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-02-11 12:06:39 +0100

    test: update snapshot

--- packages/metascraper/test/integration/itpro/index.js
+++ packages/metascraper/test/integration/itpro/index.js
@@ -14,19 +14,18 @@ const metascraper = require('../../..')([
   require('metascraper-lang')(),
   require('metascraper-logo')(),
   require('metascraper-logo-favicon')(),
+  require('metascraper-manifest')(),
   require('metascraper-publisher')(),
   require('metascraper-title')(),
   require('metascraper-url')(),
-  require('metascraper-readability')(),
-  require('metascraper-audio')()
+  require('metascraper-readability')()
 ])
 
 const url =
-  'https://player.simplecast.com/3ab2ec74-8d89-4344-9be0-a30fa53ac6a7?dark=false'
+  'https://www.itpro.com/business/leadership/dell-appoints-new-uk-channel-lead-amid-partner-program-revamp'
 
-test('simplecast', async t => {
+test('cloud-pro', async t => {
   const html = await readFile(resolve(__dirname, 'input.html'))
-  const { date, ...metadata } = await metascraper({ html, url })
-  t.is(typeof date, 'string')
+  const metadata = await metascraper({ html, url })
   t.snapshot(metadata)
 })
