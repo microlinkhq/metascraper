c9c88182db2f7fedaa6342d9318d5afcf8e6d735
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-06-25 09:06:53 +0200

    test: add snapshots

---
+++ packages/metascraper/test/integration/hola/index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const { readFile } = require('fs/promises')
+const { resolve } = require('path')
+const test = require('ava')
+
+const metascraper = require('../../..')([
+  require('metascraper-author')(),
+  require('metascraper-date')(),
+  require('metascraper-description')(),
+  require('metascraper-audio')(),
+  require('metascraper-video')(),
+  require('metascraper-image')(),
+  require('metascraper-lang')(),
+  require('metascraper-logo')(),
+  require('metascraper-logo-favicon')(),
+  require('metascraper-manifest')(),
+  require('metascraper-publisher')(),
+  require('metascraper-title')(),
+  require('metascraper-url')(),
+  require('metascraper-readability')()
+])
+
+const url =
+  'https://www.hola.com/us/celebrities/20240624701307/rauw-alejandro-bruna-marquezine-dreams-to-accomplish/'
+
+test('hola', async t => {
+  const html = await readFile(resolve(__dirname, 'input.html'))
+  const metadata = await metascraper({ html, url })
+  t.snapshot(metadata)
+})
---
+++ packages/metascraper/test/integration/sfstandard/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const { readFile } = require('fs/promises')
+const { resolve } = require('path')
+const test = require('ava')
+
+const metascraper = require('../../..')([
+  require('metascraper-author')(),
+  require('metascraper-date')(),
+  require('metascraper-description')(),
+  require('metascraper-image')(),
+  require('metascraper-lang')(),
+  require('metascraper-logo')(),
+  require('metascraper-logo-favicon')(),
+  require('metascraper-manifest')(),
+  require('metascraper-publisher')(),
+  require('metascraper-title')(),
+  require('metascraper-url')(),
+  require('metascraper-readability')()
+])
+
+const url =
+  'https://sfstandard.com/2024/06/24/service-fee-restaurants-san-francisco/'
+
+test('sfstandard', async t => {
+  const html = await readFile(resolve(__dirname, 'input.html'))
+  const metadata = await metascraper({ html, url })
+  t.snapshot(metadata)
+})
