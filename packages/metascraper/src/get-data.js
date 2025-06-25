76e0c53a5a01d0579b2014dbb1ba529340b134c3
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-06-25 21:04:48 +0200

    refactor: remove unused name prop

--- packages/metascraper/src/get-data.js
+++ packages/metascraper/src/get-data.js
@@ -3,7 +3,7 @@
 const debug = require('debug-logfmt')('metascraper:get-data')
 const { findRule, has } = require('@metascraper/helpers')
 
-const getData = async ({ rules, name, ...props }) => {
+const getData = async ({ rules, ...props }) => {
   const data = await Promise.all(
     rules.map(async ([propName, innerRules]) => {
       const duration = debug.duration()
