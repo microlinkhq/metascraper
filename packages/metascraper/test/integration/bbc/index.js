18a04fd61524b6b259cbc4b8ee9354d0fdfce50f
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-01-26 13:57:30 +0100

    test: update snapshot

--- packages/metascraper/test/integration/bbc/index.js
+++ packages/metascraper/test/integration/bbc/index.js
@@ -25,6 +25,15 @@ const url = 'http://www.bbc.com/news/business-40504764'
 
 test('bbc', async t => {
   const html = await readFile(resolve(__dirname, 'input.html'))
-  const metadata = await metascraper({ html, url })
+  const { logo, ...metadata } = await metascraper({ html, url })
+
+  t.true(
+    [
+      'https://static.files.bbci.co.uk/core/website/assets/static/icons/touch/news/touch-icon-384.728e3a03d3b09148ade1.png',
+      'https://static.files.bbci.co.uk/core/website/assets/static/icons/touch/news/touch-icon-512.685da29e9e8ce5e435a8.png'
+    ].includes(logo),
+    `Logo is not in the list: ${logo}`
+  )
+
   t.snapshot(metadata)
 })
