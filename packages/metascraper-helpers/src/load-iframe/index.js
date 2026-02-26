b78b20addfaac66e4b96f0fd1a0a5412683d9c2b
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-02-26 18:57:55 +0100

    fix(engines): align node floor with runtime requirements

--- packages/metascraper-helpers/src/load-iframe/index.js
+++ packages/metascraper-helpers/src/load-iframe/index.js
@@ -14,8 +14,8 @@ module.exports = (url, $, { timeout = 5000 } = {}) => {
     stdout: true,
     stderr: true
   })
-  const { promise, resolve, reject } = Promise.withResolvers()
-  worker.on('message', html => resolve($.load(html || '')))
-  worker.on('error', reject)
-  return promise
+  return new Promise((resolve, reject) => {
+    worker.on('message', html => resolve($.load(html || '')))
+    worker.on('error', reject)
+  })
 }
