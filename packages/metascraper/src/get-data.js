5c674af31952a85907bb8d6fdbdb15e65d9ce2aa
Jose Francisco 'Kiko' Verdú Gambín <josefrancisco.verdu@gmail.com>
2026-02-27 20:25:01 +0100

    perf(metascraper): reduce getData allocations (#827)

--- packages/metascraper/src/get-data.js
+++ packages/metascraper/src/get-data.js
@@ -4,7 +4,9 @@ const debug = require('debug-logfmt')('metascraper:get-data')
 const { findRule, has } = require('@metascraper/helpers')
 
 const getData = async ({ rules, ...props }) => {
-  const data = await Promise.all(
+  const data = {}
+
+  await Promise.all(
     rules.map(async ([propName, innerRules]) => {
       const duration = debug.duration()
       let normalizedValue = null
@@ -26,11 +28,11 @@ const getData = async ({ rules, ...props }) => {
       duration(
         `${propName}=${normalizedValue} rules=${innerRules.length} status=${status}`
       )
-      return [propName, normalizedValue]
+      data[propName] = normalizedValue
     })
   )
 
-  return Object.fromEntries(data)
+  return data
 }
 
 module.exports = getData
