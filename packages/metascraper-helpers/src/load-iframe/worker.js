57e005b54f82794bfcd6a041579f4becf6d22362
Kiko Beats <josefrancisco.verdu@gmail.com>
2026-06-09 21:09:12 +0200

    chore: update dependencies

--- packages/metascraper-helpers/src/load-iframe/worker.js
+++ packages/metascraper-helpers/src/load-iframe/worker.js
@@ -1,14 +1,30 @@
 'use strict'
 
 const { workerData, parentPort } = require('node:worker_threads')
-const { JSDOM, VirtualConsole } = require('jsdom')
+const { JSDOM, VirtualConsole, requestInterceptor } = require('jsdom')
+
+const iframeInterceptor = requestInterceptor(async (request, { element }) => {
+  if (element?.localName !== 'iframe') return
+
+  const response = await fetch(request)
+  if (!response.ok) return new Response('', { status: response.status })
+
+  const headers = new Headers(response.headers)
+  headers.delete('content-encoding')
+  headers.delete('content-length')
+  return new Response(response.body, {
+    headers,
+    status: response.status,
+    statusText: response.statusText
+  })
+})
 
 async function main ({ url, html, timeout }) {
   const dom = new JSDOM(html, {
     url,
     virtualConsole: new VirtualConsole(),
     runScripts: 'dangerously',
-    resources: 'usable'
+    resources: { interceptors: [iframeInterceptor] }
   })
 
   const iframe = dom.window.document.querySelector('iframe')
